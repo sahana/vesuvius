5c6651a88333d9e5a851cbfdd9a7f2ddbcbfb499
damitha23 <>
2005-01-09 08:47:17 +0000

    added consignment TO objects

---
+++ sahana-phase1/transport/src/org/transport/business/ConsignmentItemTO.java
@@ -0,0 +1,93 @@
+package org.transport.business;
+
+import java.sql.Date;
+
+/**
+ * Copyright 2001-2004 The Apache Software Foundation.
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class ConsignmentItemTO {
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public int getConsignmentId() {
+        return consignmentId;
+    }
+
+    public void setConsignmentId(int consignmentId) {
+        this.consignmentId = consignmentId;
+    }
+
+    public int getIssuedQty() {
+        return issuedQty;
+    }
+
+    public void setIssuedQty(int issuedQty) {
+        this.issuedQty = issuedQty;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getUoM() {
+        return UoM;
+    }
+
+    public void setUoM(String uoM) {
+        UoM = uoM;
+    }
+
+    public String getConversion() {
+        return conversion;
+    }
+
+    public void setConversion(String conversion) {
+        this.conversion = conversion;
+    }
+
+    public Date getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(Date expDate) {
+        this.expDate = expDate;
+    }
+
+    public int getAcceptedQty() {
+        return acceptedQty;
+    }
+
+    public void setAcceptedQty(int acceptedQty) {
+        this.acceptedQty = acceptedQty;
+    }
+
+    private String itemCode;
+    private int consignmentId;
+    private String comments;
+    private int issuedQty;
+    private String UoM;
+    private String conversion;
+    private Date expDate;
+    private int acceptedQty;
+}
