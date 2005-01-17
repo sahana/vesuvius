6ce10911612ab80cd5f5c6ee48e1de614e1d9a92
ajithranabahu <>
2005-01-17 11:37:07 +0000

    Made a correction to comply with the mysql 4.0.22

--- sahana-phase1/orgreg/sql/db_patch.sql
+++ sahana-phase1/orgreg/sql/db_patch.sql
@@ -2,7 +2,7 @@ CREATE TABLE `organization_district` (
   `OrgCode` varchar(100) NOT NULL default '',
   `DistrictName` varchar(100) NOT NULL default '',
   PRIMARY KEY  (`DistrictName`,`OrgCode`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1; 
+) TYPE=MyISAM DEFAULT CHARSET=latin1; 
 
 
 ALTER TABLE organization ADD COLUMN LastUpdate varchar(100) NOT NULL;
