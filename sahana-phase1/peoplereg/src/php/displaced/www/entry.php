eb2c8ded2a4de49691561bc44397b87f102b2564
Anuradha Ratnaweera <>
2005-01-17 19:58:25 +0000

    Major code cleanup; the diff to previous revision may not make much sense

--- sahana-phase1/peoplereg/src/php/displaced/www/entry.php
+++ sahana-phase1/peoplereg/src/php/displaced/www/entry.php
@@ -1,53 +1,60 @@
 <?
 
-/* This code works well, but very dirty :-(  What do do? A million records
-   are arriving tomorrow, and this UI is going to be used to enter data.
-   -- Anuradha */
+// This code works well, but very dirty :-(  What do do? A million records
+// are arriving tomorrow, and this UI is going to be used to enter data.
+// -- Anuradha
 
 session_start();
 require('forms.php');
 require('db.php');
 
+// Need to generate this dynamically
 $num_members = 10; /* FIXME: get rid of the static value */
 
 ?>
 <html>
 <head>
-<title>Enter displaced persons</title>
-<link href="entry.css" rel="stylesheet" type="text/css" /> 
+	<title>Displaced Persons Data Entry</title>
+	<link href="entry.css" rel="stylesheet" type="text/css" /> 
 </head>
-
 <body>
-
-<form action="entry.php" method="post">
-
-<input type="hidden" name="num_members" value="10" />
+	<form action="entry.php" method="post">
+	<input type="hidden" name="num_members" value="10" />
 
 <?
 
+// Connect to DB running locally
 $dbh = mysql_connect('localhost', 'apache', 'abcd321')
-   or die('Could not connect: ' . mysql_error());
-
+	or die('Could not connect: ' . mysql_error());
 mysql_select_db('mambo') or die('Could not select database');
 
+// By default, we are in the data entry mode
 $screen = 'entry';
 
+// Do something extra for POST
 if ($_POST) {
 	if ($_POST['back']) {
+		// Back button is pressed
 		$screen = 'entry';
 	}
 	elseif ($_POST['submit']) {
+		// Submit button pressed from the first screen
+		// Capture form data into the session
+
+		// Common information
 		capture_input_string('district');
 		capture_input_string('division');
 		capture_input_string('grama');
 		capture_input_string('village');
 
+		// General family information
 		capture_input_int('family_serial_no');
 		capture_input_string('address');
 		capture_input_int('num_males');
 		capture_input_int('num_females');
 		capture_input_int('num_children');
 
+		// Family members and income information
 		capture_input_int('num_members');
 		$num_members = $_SESSION['form']['num_members'];
 		for ($i = 0; $i < $num_members; $i++) {
@@ -58,328 +65,268 @@ if ($_POST) {
 		}
 		capture_input_int('other_income');
 		capture_input_int('total_income');
-
 		capture_input_string('property_owned');
 		capture_input_int('property_value');
 
+		// Present status and relief related information
 		capture_input_string('current_location');
 		capture_input_string('current_camp');
-
 		capture_input_int('relief_adults');
 		capture_input_int('relief_children');
-
 		capture_input_string('relief_period');
 		capture_input_string('remarks');
 
+		// Confirmation screen is to be displayed
 		$screen = 'confirm';
 	}
 }
 
-if ($screen == 'confirm') {
+// Display data entry screen
+if ($screen == 'entry') {
 
 ?>
+	<h2>Family Details Entry Form</h2>
+	<div align="center">
+		<hr />
+		<table class="entry">
+			<tr>
+				<td>District:</td>
+				<td><? show_input_select($dbh, 'district'); ?></td>
+				<td>Divisional secretariat:</td>
+				<td><? show_input_text('division'); ?></td>
+			</tr>
+			<tr>
+				<td>Grama Niladhari's Division:</td>
+				<td><? show_input_text('grama'); ?></td>
+				<td>Village:</td>
+				<td><? show_input_text('village'); ?></td>
+			</tr>
+		</table>
+		<hr />
+		<table class="entry">
+			<tr>
+				<td>Serial number of the family:</td>
+				<td><? show_input_text('family_serial_no'); ?></td>
+			</tr>
+			<tr>
+				<td>Address:</td>
+				<td><? show_input_text('address', 70); ?></td>
+			</tr>
+		</table>
+		<table class="entry">
+			<tr>
+				<td>Males: <? show_input_text('num_males'); ?></td>
+				<td>Females: <? show_input_text('num_females'); ?></td>
+				<td>Children: <? show_input_text('num_children'); ?></td>
+			</tr>
+		</table>
+		<table class="entry">
+			<tr>
+				<td align="center">Name</td>
+				<td align="center">Status</td>
+				<td align="center">Occupation</td>
+				<td align="center">Income</td>
+			</tr>
+<?
 
-<h2>Confirmation Form</h2>
+// Inputs for individual family members
+for ($i = 0; $i < $num_members; $i++) {
 
-<hr />
+?>
+			<tr>
+				<td align="center"><? show_input_element_text('name', $i, 50); ?></td>
+				<td align="center"><? show_input_element_select($dbh, 'status', $i); ?></td>
+				<td align="center"><? show_input_element_text('occupation', $i); ?></td>
+				<td align="center"><? show_input_element_text('income', $i); ?></td>
+			</tr>
+<?
 
-<div align="center">
-<table class="entry">
-<tr>
-  <td>District:</td>
-  <td><? display_input_caption('district'); ?></td>
-  <td>Divisional secretariat:</td>
-  <td><? display_input('division'); ?></td>
-</tr>
-<tr>
-  <td>Grama Niladhari's Division:</td>
-  <td><? display_input('grama'); ?></td>
-  <td>Village:</td>
-  <td><? display_input('village'); ?></td>
-</tr>
+} // end for
 
-</table>
-</div>
+?>
+			<tr>
+				<td colspan="3" align="right">Other income:</td>
+				<td align="center"><? show_input_text('other_income'); ?></td>
+			</tr>
+			<tr>
+				<td colspan="3" align="right">Total income:</td>
+				<td align="center"><? show_input_text('total_income'); ?></td>
+			</tr>
+		</table>
+		<table class="entry">
+			<tr>
+				<td valign="middle">Property owned:</td>
+				<td><? show_input_textarea('property_owned', 3, 50); ?></td>
+				<td valign="middle">Value (Rs): <? show_input_text('property_value'); ?></td>
+			</tr>
+		</table>
+		<hr />
+		<table class="entry">
+			<tr>
+				<td>Current location: <? show_input_text('current_location'); ?></td>
+				<td>Camp (if applicable): <? show_input_text('current_camp'); ?></td>
+			</tr>
+		</table>
+		<table class="entry">
+			<tr>
+				<td>No of people eligible for relief:</td>
+				<td>Adults: <? show_input_text('relief_adults'); ?></td>
+				<td>Children: <? show_input_text('relief_children'); ?></td>
+			</tr>
+		</table>
+		<table class="entry">
+			<tr>
+				<td>Probable period for which relief is necessary: <? show_input_text('relief_period'); ?></td>
+			</tr>
+		</table>
+		<table class="entry">
+			<tr>
+				<td>Remarks:</td>
+				<td><? show_input_textarea('remarks', 3, 50); ?></td>
+			</tr>
+		</table>
+		<table class="entry">
+			<tr>
+				<td align="right"><input type="submit" name="submit" value=" Submit " /></td>
+			</tr>
+		</table>
+	</div>
+<?
 
-<hr />
+} // if ($screen == 'entry')
 
-<div align="center">
-<table class="entry">
-<tr>
-<td>Serial number of the family:</td><td><? display_input('family_serial_no'); ?></td>
-</tr>
-<tr>
-<td>Address:</td><td><? display_input('address'); ?></td>
-</tr>
-</table>
-</div>
+// Done with the data entry screen
+// Now to the confirmation screen
 
-<div align="center">
-<table class="entry">
-<tr>
-<td>Males: <? display_input('num_males'); ?>, Females: <? display_input('num_females'); ?>, Children: <? display_input('num_children'); ?></td>
-</tr>
-</table>
-</div>
+elseif ($screen == 'confirm') {
 
-<div align="center">
-<table class="entry">
-
-<tr>
-<td align="center">Name</td>
-<td align="center">Status</td>
-<td align="center">Occupation</td>
-<td align="center">Income</td>
-</tr>
-
+?>
+	<h2>Confirmation Form</h2>
+	<div align="center">
+		<hr />
+		<table class="entry">
+			<tr>
+				<td>District:</td>
+				<td><? display_input_caption('district'); ?></td>
+				<td>Divisional secretariat:</td>
+				<td><? display_input('division'); ?></td>
+			</tr>
+			<tr>
+				<td>Grama Niladhari's Division:</td>
+				<td><? display_input('grama'); ?></td>
+				<td>Village:</td>
+				<td><? display_input('village'); ?></td>
+			</tr>
+		</table>
+		<hr />
+		<table class="entry">
+			<tr>
+				<td>Serial number of the family:</td>
+				<td><? display_input('family_serial_no'); ?></td>
+			</tr>
+			<tr>
+				<td>Address:</td>
+				<td><? display_input('address'); ?></td>
+			</tr>
+		</table>
+		<table class="entry">
+			<tr>
+				<td>Males: <? display_input('num_males'); ?>, Females: <? display_input('num_females'); ?>, Children: <? display_input('num_children'); ?></td>
+			</tr>
+		</table>
+		<table class="entry">
+			<tr>
+				<td align="center">Name</td>
+				<td align="center">Status</td>
+				<td align="center">Occupation</td>
+				<td align="center">Income</td>
+			</tr>
 <?
  
 for ($i = 0; $i < $num_members; $i++) {
 	if (member_info_available($i)) {
-		print '<tr><td>';
+		print "\t\t\t<tr>\n";
+		print "\t\t\t\t<td>";
 		display_input_element('name', $i);
-		print '</td><td align="center">';
+		print "</td>\n";
+		print "\t\t\t\t<td>";
 		display_input_element_caption('status', $i);
-		print '</td><td align="center">';
+		print "</td>\n";
+		print "\t\t\t\t<td>";
 		display_input_element('occupation', $i);
-		print '</td><td align="right">';
+		print "</td>\n";
+		print "\t\t\t\t<td>";
 		display_input_element_money('income', $i);
-		print '</td></tr>';
+		print "</td>\n";
+		print "\t\t\t<tr>\n";
 	}
 }
+
 ?>
-
-<tr>
-<td colspan="3" align="right">Other income</td>
-<td align="right"><? display_input_money('other_income'); ?></td>
-</tr>
-
-<tr>
-<td colspan="3" align="right">Total income</td>
-<td align="right"><? display_input_money('total_income'); ?></td>
-</tr>
-
-</table>
-
-<table class="entry">
-<tr>
-<td>Property owned:</td>
-<td><? display_input('property_owned'); ?></td>
-<td>Property value:</td>
-<td><? display_input_money('property_value'); ?></td>
-</tr>
-</table>
-</div>
-
-
-<hr />
-
-<div align="center">
-<table class="entry">
-<tr>
-<td>Current location:</td>
-<td><? display_input('current_location'); ?></td>
-<td>Camp (if applicable):</td>
-<td><? display_input('current_camp'); ?></td>
-</tr>
-</table>
-</div>
-
-<div align="center">
-<table class="entry">
-<tr>
-<td>No of people eligible for relief:</td>
-<td>Adults:</td>
-<td><? display_input('relief_adults'); ?></td>
-<td>Children:</td>
-<td><? display_input('relief_children'); ?></td>
-</tr>
-</table>
-</div>
-
-<div align="center">
-<table class="entry">
-<tr>
-<td>Probable period for which relief is necessary: <? display_input('relief_period'); ?></td>
-</tr>
-</table>
-</div>
-
-
-<div align="center">
-<table class="entry">
-<tr>
-<td>Remarks:</td>
-<td><? display_input('remarks', 3, 50); ?></td>
-</tr>
-</table>
-</div>
-
-<div align="center">
-<table class="entry">
-<tr>
-<td align="right"><input type="submit" name="back" value=" Back " />
-<input type="submit" name="confirm" value=" Confirm " /></td>
-</tr>
-</table>
-</div>
-
-
+			<tr>
+				<td colspan="3" align="right">Other income</td>
+				<td align="right"><? display_input_money('other_income'); ?></td>
+			</tr>
+			<tr>
+				<td colspan="3" align="right">Total income</td>
+				<td align="right"><? display_input_money('total_income'); ?></td>
+			</tr>
+		</table>
+		<table class="entry">
+			<tr>
+				<td>Property owned:</td>
+				<td><? display_input('property_owned'); ?></td>
+				<td>Property value:</td>
+				<td><? display_input_money('property_value'); ?></td>
+			</tr>
+		</table>
+		<hr />
+		<table class="entry">
+			<tr>
+				<td>Current location:</td>
+				<td><? display_input('current_location'); ?></td>
+				<td>Camp (if applicable):</td>
+				<td><? display_input('current_camp'); ?></td>
+			</tr>
+		</table>
+		<table class="entry">
+			<tr>
+				<td>No of people eligible for relief:</td>
+				<td>Adults:</td>
+				<td><? display_input('relief_adults'); ?></td>
+				<td>Children:</td>
+				<td><? display_input('relief_children'); ?></td>
+			</tr>
+		</table>
+		<table class="entry">
+			<tr>
+				<td>Probable period for which relief is necessary: <? display_input('relief_period'); ?></td>
+			</tr>
+		</table>
+		<table class="entry">
+			<tr>
+				<td>Remarks:</td>
+				<td><? display_input('remarks', 3, 50); ?></td>
+			</tr>
+		</table>
+		<table class="entry">
+			<tr>
+				<td align="right"><input type="submit" name="back" value=" Back " />
+				<input type="submit" name="confirm" value=" Confirm " /></td>
+			</tr>
+		</table>
+	</div>
 <?
 
-}
-else {
+} // elseif ($screen == 'confirm')
 
 ?>
-
-<h2>Family Details Entry Form</h2>
-
-<hr />
-
-<div align="center">
-<table class="entry">
-<tr>
-  <td>District:</td>
-  <td><? show_input_select($dbh, 'district'); ?></td>
-  <td>Divisional secretariat:</td>
-  <td><? show_input_text('division'); ?></td>
-</tr>
-<tr>
-  <td>Grama Niladhari's Division:</td>
-  <td><? show_input_text('grama'); ?></td>
-  <td>Village:</td>
-  <td><? show_input_text('village'); ?></td>
-</tr>
-
-</table>
-</div>
-
-<hr />
-
-<div align="center">
-<table class="entry">
-<tr>
-<td>Serial number of the family:</td><td><? show_input_text('family_serial_no'); ?></td>
-</tr>
-<tr>
-<td>Address:</td><td><? show_input_text('address', 70); ?></td>
-</tr>
-</table>
-</div>
-
-<div align="center">
-<table class="entry">
-<tr>
-<td>Males: <? show_input_text('num_males'); ?></td>
-<td>Females: <? show_input_text('num_females'); ?></td>
-<td>Children: <? show_input_text('num_children'); ?></td>
-</tr>
-</table>
-</div>
-
-<div align="center">
-<table class="entry">
-
-<tr>
-<td align="center">Name</td>
-<td align="center">Status</td>
-<td align="center">Occupation</td>
-<td align="center">Income</td>
-</tr>
-
-<?
-for ($i = 0; $i < $num_members; $i++) {
-?>
-
-<tr>
-<td align="center"><? show_input_element_text('name', $i, 50); ?></td>
-<td align="center"><? show_input_element_select($dbh, 'status', $i); ?></td>
-<td align="center"><? show_input_element_text('occupation', $i); ?></td>
-<td align="center"><? show_input_element_text('income', $i); ?></td>
-</tr>
-
-<?
-}
-?>
-
-<tr>
-<td colspan="3" align="right">Other income:</td>
-<td align="center"><? show_input_text('other_income'); ?></td>
-</tr>
-
-<tr>
-<td colspan="3" align="right">Total income:</td>
-<td align="center"><? show_input_text('total_income'); ?></td>
-</tr>
-
-</table>
-</div>
-
-<div align="center">
-<table class="entry">
-<tr>
-<td valign="middle">Property owned:</td><td><? show_input_textarea('property_owned', 3, 50); ?></td>
-<td valign="middle">Value (Rs): <? show_input_text('property_value'); ?></td>
-</tr>
-</table>
-</div>
-
-<hr />
-
-<div align="center">
-<table class="entry">
-<tr>
-<td>Current location: <? show_input_text('current_location'); ?></td>
-<td>Camp (if applicable): <? show_input_text('current_camp'); ?></td>
-</tr>
-</table>
-</div>
-
-<div align="center">
-<table class="entry">
-<tr>
-<td>No of people eligible for relief:</td>
-<td>Adults: <? show_input_text('relief_adults'); ?></td>
-<td>Children: <? show_input_text('relief_children'); ?></td>
-</tr>
-</table>
-</div>
-
-<div align="center">
-<table class="entry">
-<tr>
-<td>Probable period for which relief is necessary: <? show_input_text('relief_period'); ?></td>
-</tr>
-</table>
-</div>
-
-<div align="center">
-<table class="entry">
-<tr>
-<td>Remarks:</td>
-<td><? show_input_textarea('remarks', 3, 50); ?></td>
-</tr>
-</table>
-</div>
-
-<div align="center">
-<table class="entry">
-<tr>
-<td align="right"><input type="submit" name="submit" value=" Submit " /></td>
-</tr>
-</table>
-</div>
-
-<?
-}
-?>
-
-</form>
-
+	</form>
 <body>
 </html>
 
 <?
+
 function member_info_available($n)
 {
 	global $_SESSION;
