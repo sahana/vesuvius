62e6a9d34ed00893c12b628c38d8009884c30e08
deepal <>
2005-01-17 17:37:57 +0000

    to create orr_sector

--- sahana-phase1/orgreg/sql/db_patch.sql
+++ sahana-phase1/orgreg/sql/db_patch.sql
@@ -10,3 +10,15 @@ ALTER TABLE organization ADD COLUMN LastUpdate varchar(100) NOT NULL;
 ALTER TABLE organization ADD COLUMN UntilDate varchar(100) NOT NULL;
 
 ALTER TABLE organization ADD COLUMN IsSriLankan tinyint NOT NULL;
+
+# Host: 192.168.101.14
+# Database: erms
+# Table: 'organization_sector'
+# 
+CREATE TABLE `organization_sector` (
+  `OrgCode` varchar(100) NOT NULL default '',
+  `Sector` varchar(100) NOT NULL default '',
+  PRIMARY KEY  (`OrgCode`,`Sector`)
+) Type = MyISAM DEFAULT CHARSET=latin1; 
+
+
