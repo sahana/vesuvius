9d26bfac28de8b3d8eb0342b2367fdaab34f2ef9
hemapani <>
2005-01-18 14:05:00 +0000

    fix the missing return

--- sahana-phase1/rms/web/comman/validate.js
+++ sahana-phase1/rms/web/comman/validate.js
@@ -14,17 +14,21 @@ var quantitiyStr = document.form1.quantity.value;
         document.form1.submitType.value = "Save";
         if(document.form1.districts.value == "deafult"){
             alert("Please Select a value for District");
+            return;
         }
         if(document.form1.siteType.value == "deafult"){
             alert("Please Select a value for Site Type");
+            return;
         }
     }else if (type==2){
         document.form1.submitType.value = "AddList";
         if(document.form1.catogories.value == "deafult"){
             alert("Please Select a value for Catogories");
+            return;
         }
         if(document.form1.priorities.value == "deafult"){
             alert("Please Select a value for Priorities");
+            return;
         }
     }else if (type==3){
         document.form1.submitType.value = "Clear";
