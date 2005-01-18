83683b229c9a42336c67ffdc8acef0de09d55c8c
Buddhika <>
2005-01-18 07:49:04 +0000

    Displaced module initial commit

---
+++ sahana-phase1/peoplereg/mambo/modules/mod_displaced/mod_displaced.php
@@ -0,0 +1,11 @@
+<?php
+ # mod_displaced.php - Displaced persons module for mambo.
+ # Copyright : Virtusa Corporation
+ # License : GPL
+ # Author : Buddhika Siddhisena
+ # Created: 18/01/2005
+ # Updated: 18/01/2005
+defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
+
+ include_once("$mosConfig_absolute_path/../peoplereg/displaced/entry.php");
+?>
