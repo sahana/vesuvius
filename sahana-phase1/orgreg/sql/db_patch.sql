8d264aee4dc1a81676a333cda172be5e9b92f7fb
ajithranabahu <>
2005-01-18 13:13:05 +0000

    made some minor modifications

--- sahana-phase1/orgreg/sql/db_patch.sql
+++ sahana-phase1/orgreg/sql/db_patch.sql
@@ -2,13 +2,11 @@ CREATE TABLE `organization_district` (
   `OrgCode` varchar(100) NOT NULL default '',
   `DistrictName` varchar(100) NOT NULL default '',
   PRIMARY KEY  (`DistrictName`,`OrgCode`)
-) TYPE=MyISAM DEFAULT CHARSET=latin1; 
+) TYPE=MyISAM; 
 
 
 ALTER TABLE organization ADD COLUMN LastUpdate varchar(100) NOT NULL;
-
 ALTER TABLE organization ADD COLUMN UntilDate varchar(100) NOT NULL;
-
 ALTER TABLE organization ADD COLUMN IsSriLankan tinyint NOT NULL;
 
 # Host: 192.168.101.14
@@ -19,6 +17,6 @@ CREATE TABLE `organization_sector` (
   `OrgCode` varchar(100) NOT NULL default '',
   `Sector` varchar(100) NOT NULL default '',
   PRIMARY KEY  (`OrgCode`,`Sector`)
-) Type=MyISAM DEFAULT CHARSET=latin1; 
+) TYPE=MyISAM; 
 
 
