91032ea8a36ffbabba3db6450fe3a2cc2459c997
Buddhika <>
2005-01-18 13:16:05 +0000

    Added ACL support so that only authorized users can add

--- sahana-phase1/peoplereg/mambo/modules/mod_displaced/mod_displaced.php
+++ sahana-phase1/peoplereg/mambo/modules/mod_displaced/mod_displaced.php
@@ -6,6 +6,8 @@
  # Created: 18/01/2005
  # Updated: 18/01/2005
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
-
+include_once("$mosConfig_absolute_path/acl/inc_acl.php");
+if(checkacl($my->username,"People Registry","ADD")){
  include_once("$mosConfig_absolute_path/../peoplereg/displaced/entry.php");
+}
 ?>
