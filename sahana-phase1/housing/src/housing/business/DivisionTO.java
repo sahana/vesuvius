25bc5a8674687e232f0f46322dc0c5f29903f457
nithyakala <>
2005-01-19 07:13:03 +0000

    Java Beans for Housing Scheme

---
+++ sahana-phase1/housing/src/housing/business/DivisionTO.java
@@ -0,0 +1,50 @@
+package org.housing.business;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nithyakala
+ * Date: Jan 18, 2005
+ * Time: 7:33:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DivisionTO {
+    private String divId;
+    private String divName;
+    private String distCode;
+
+
+      public DivisionTO(){
+          
+      }
+
+      public DivisionTO (String divId,String divName, String distCode) {
+            this.divId =divId;
+            this.divName = divName;
+            this.distCode = distCode;
+        }
+
+        public String getDivId() {
+            return divId;
+        }
+
+        public void setDivId(String divId) {
+            this.divId = divId;
+        }
+
+        public String getDivName() {
+            return divName;
+        }
+
+        public void setDivName(String divName) {
+            this.divName = divName;
+        }
+
+        public String getDistCode(){
+            return distCode;
+        }
+
+        public void setDistCode(String distCode){
+            this.distCode = distCode;
+        }
+
+}
