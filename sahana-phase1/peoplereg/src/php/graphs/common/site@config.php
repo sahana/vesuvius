a79887649ec9d5beb05f9fb2464c87a2550e97c8
Buddhika <>
2005-01-20 11:49:48 +0000

    Added support for daily report tables

--- sahana-phase1/peoplereg/src/php/graphs/common/site@config.php
+++ sahana-phase1/peoplereg/src/php/graphs/common/site@config.php
@@ -2,7 +2,7 @@
  # site@config.php - Side wide configuration info.
  # Author: Buddhika Siddhisena [Bud@babytux.org]
  # Created: 31/12/2004
- # Updated: 03/01/2005
+ # Updated: 17/01/2005
 
 $webroot = '/var/www/mambo/sahana';
 
@@ -11,14 +11,13 @@ $docroot = '/mambo/sahana';
 
 #-- print debug info --
 $db=1; $xdb=1;
-function db($msg){global $db;if($db) echo("<font color=\"#EE0000\">$msg</font>");}
-function xdb($msg){global $xdb;if($xdb) echo("<font color=\"#1100BB\">$msg</font>");}
+function db($msg){global $db;if($db) echo("<br><font color=\"#EE0000\">$msg</font>");}
+function xdb($msg){global $xdb;if($xdb) echo("<br><font color=\"#1100BB\">$msg</font>");}
 
 
 #-- database config --
 $host = "localhost";
 $dbuser="dbuser"; $dbpassword="dbpassword";
-
 
 $database="mambo";
 
@@ -30,10 +29,16 @@ $attroptiontable = 'sahana_attribute_options';
 $attroptiontable_fields = 'id,attribute_id,name,caption';
 $entitytable = 'sahana_entities';
 $entitytable_fields = 'id';
+$dailyreporttable='sahana_daily_report';
+$dailyreporttable_fields='report_id,created,title,field_heading';
+$dailyreportdatatable='sahana_dailyreport_data';
+$dailyreportdatatable_fields='report_id,field_data';
+
 
 $resperpage=10; #results per page
 
 #-- Application Config
+$rowaltcolor='#EEEEEE';
 $maxstatsize = 5; # Maximum number of statics labels to show
 
 ?>
