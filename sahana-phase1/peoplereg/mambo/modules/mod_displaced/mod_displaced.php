f2dbc090ae8db728a213898885fe32a7135f50e1
Buddhika <>
2005-01-21 16:53:10 +0000

    Modified to check whether user is logged on and if not display message

--- sahana-phase1/peoplereg/mambo/modules/mod_displaced/mod_displaced.php
+++ sahana-phase1/peoplereg/mambo/modules/mod_displaced/mod_displaced.php
@@ -4,10 +4,11 @@
  # License : GPL
  # Author : Buddhika Siddhisena
  # Created: 18/01/2005
- # Updated: 18/01/2005
+ # Updated: 21/01/2005
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
-include_once("$mosConfig_absolute_path/acl/inc_acl.php");
-if(checkacl($my->username,"People Registry","ADD")){
+if($my->username){
  include_once("$mosConfig_absolute_path/../peoplereg/displaced/entry.php");
+}else{
+ print "Your session has expired! Please login again.";
 }
 ?>
