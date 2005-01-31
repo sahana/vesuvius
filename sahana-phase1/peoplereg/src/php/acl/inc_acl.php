ea5501df314e2feda52b0c3e68ee2874ebafef6e
Buddhika <>
2005-01-31 10:15:48 +0000

    Fixed a logical bug with the way multiple accesslevels worked

--- sahana-phase1/peoplereg/src/php/acl/inc_acl.php
+++ sahana-phase1/peoplereg/src/php/acl/inc_acl.php
@@ -3,13 +3,13 @@
  # Author: Buddhika Siddhisena [Bud@babytux.org]
  # License : GPL
  # Created: 11/01/2005
- # Updated: 18/01/2005
+ # Updated: 31/01/2005
 
 function checkacl($user,$module_name,$access_levels){
  
  #-- database config --
 $host = "localhost";
-$dbuser="dbuser"; $dbpassword="dbpassword";
+$dbuser="root"; $dbpassword="";
 
 $database="erms";
 
@@ -37,18 +37,13 @@ if (!mysql_select_db($database,$dbconn)){
 
 $sql="SELECT AccessLevel,Permission FROM $accesspermissions a,  $userrolestable b, $accessmodulestable c WHERE b.RoleId=a.RoleId AND a.ModuleId=c.ModuleId AND c.ModuleName LIKE '$module_name' AND UserName LIKE '$user'";
 
-if(preg_match("/[\,]+/",$access_levels)){ # multiple access levels
-   $sql.=" AND AccessLevel IN ('".preg_replace("/\,/","','",$access_levels)."')";
-}else{
-   $sql.=" AND AccessLevel='$access_levels'";
-}
 $rs=mysql_query($sql);
 $status=true;
 if(mysql_num_rows($rs)==0){$status=false;} # If we didnt get any result we deny
 
 for($i=0;$i<mysql_num_rows($rs);$i++){
   $row=mysql_fetch_array($rs);
-  if (preg_match("/^n/i",$row['Permission'])){$status=false;}
+  if (!(preg_match("/$row[AccessLevel]/",$access_levels)) || preg_match("/^n/i",$row['Permission'])){$status=false;}
 }
 
 mysql_close($dbconn);
