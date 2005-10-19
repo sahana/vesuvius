bac089823abd51245b102aae09d2c88f331bbc4b
Janaka S B Wickramasinghe <>
2005-10-19 06:13:48 +0000

    The people reg method2 is added.

--- sahana-phase2/inst/mysql.sql
+++ sahana-phase2/inst/mysql.sql
@@ -1,8 +1,5 @@
-/* CORE SCHEMA */
-/* prefix : cre_ */
-
 -- SESSIONS
-CREATE TABLE cre_sessions(
+CREATE TABLE sessions(
 	sesskey VARCHAR(32) NOT NULL,
 	expiry INT NOT NULL,
 	expireref VARCHAR(64),
@@ -10,130 +7,64 @@ CREATE TABLE cre_sessions(
 	PRIMARY KEY (sesskey)
 );
 
-
--- PEOPLE
-CREATE TABLE cre_people(
-	id BIGINT NOT NULL AUTO_INCREMENT,
-	firstname VARCHAR(40),
-	lastame VARCHAR(100),
-    othernames TEXT,
-    email VARCHAR(255),
-    address TEXT,
-	PRIMARY KEY (id)
-);
-
--- USERS
-CREATE TABLE cre_users(
-    userid BIGINT NOT NULL,
-	username VARCHAR(10) NOT NULL,
-	password VARCHAR(40) NOT NULL,
-	PRIMARY KEY (userid),
-	FOREIGN KEY (userid) REFERENCES cre_people(id)
-);
-
-/* CORE LOG SCHEMA */
-
-	-- USER LOGS
-	CREATE TABLE cre_userlog(
-		user_logid BIGINT NOT NULL AUTO_INCREMENT,
-		userid BIGINT NOT NULL,
-		ip VARCHAR( 15 ) NOT NULL,
-		logintime TIMESTAMP NOT NULL DEFAULT NOW(),
-		PRIMARY KEY(user_logid),
-		FOREIGN KEY (userid) REFERENCES cre_users (userid)
-	);
-	
-	-- ERROR LOG
-	CREATE TABLE cre_errorlog(
-		error_logid BIGINT NOT NULL AUTO_INCREMENT,
-		user_logid BIGINT NOT NULL,
-		severity VARCHAR(10) NOT NULL,
-		file TEXT  NOT NULL,
-		time TIMESTAMP NOT NULL DEFAULT NOW(),
-		message TEXT NOT NULL,
-		line INT NOT NULL,
-		PRIMARY KEY (error_logid),
-		FOREIGN KEY (user_logid) REFERENCES cre_userlog(user_logid)
-	);
-	
-	-- ACTION LOG 
-	CREATE TABLE cre_actionlog(
-	    action_logid BIGINT NOT NULL AUTO_INCREMENT,
-	    userid BIGINT NOT NULL,
-	    user_logid BIGINT NOT NULL,
-	    message TEXT NOT NULL,
-	    loglevel VARCHAR(10) NOT NULL,
-        module VARCHAR(50) NOT NULL,
-	    date timestamp NOT NULL DEFAULT NOW(),
-	    PRIMARY KEY (action_logid),
-	    FOREIGN KEY (user_logid) REFERENCES cre_userlog(user_logid)
-	);
-	
-	-- DEBUG LOG
-	CREATE TABLE cre_debuglog(
-	    debug_logid BIGINT NOT NULL AUTO_INCREMENT,
-	    userid BIGINT NOT NULL,
-	    user_logid BIGINT NOT NULL,
-	    action VARCHAR (100) NOT NULL,
-	    modulename VARCHAR(50) NOT NULL,
-	    date timestamp NOT NULL DEFAULT NOW(),
-	    data TEXT,
-	    PRIMARY KEY (debug_logid),
-	    FOREIGN KEY (user_logid) REFERENCES cre_userlog(user_logid)
-	);
-	
 /**** CORE LOG SCHEMA END *****/
 
 -- MODULES
-CREATE TABLE cre_modules(
-	moduleid BIGINT NOT NULL AUTO_INCREMENT,
+CREATE TABLE modules(
+	module_id BIGINT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(50) NOT NULL,
 	description TEXT,
 	version VARCHAR(10) NOT NULL,
-	active BOOL NOT NULL DEFAULT 0 ,
-	PRIMARY KEY (moduleid)
+	active BOOL NOT NULL DEFAULT 0,
+	PRIMARY KEY (module_id)
 );
 
+
 --CUSTOM MODULE CONFIGURATIONS
-CREATE TABLE cre_config(
-	configid BIGINT NOT NULL AUTO_INCREMENT,
+CREATE TABLE config(
+	config_id BIGINT NOT NULL AUTO_INCREMENT,
 	config_group VARCHAR(100),
 	name VARCHAR(50) NOT NULL,
 	value TEXT,
 	description TEXT,
 	type VARCHAR(10),
-	moduleid BIGINT,
-	PRIMARY KEY(configid),
-	FOREIGN KEY (moduleid) REFERENCES cre_modules (moduleid)
+	module_id BIGINT,
+	PRIMARY KEY(config_id),
+	FOREIGN KEY (module_id) REFERENCES modules (module_id)
 );
 
+
 --CUSTOM CONFIGURATION LISTS (SELECT)
-CREATE TABLE cre_configlist(
+CREATE TABLE configlist(
 	description TEXT NOT NULL,
 	value VARCHAR(50),
-	configid BIGINT NOT NULL,
-	FOREIGN KEY (configid) REFERENCES cre_config (configid)
+	config_id BIGINT NOT NULL,
+	FOREIGN KEY (config_id) REFERENCES config (config_id)
 );
+
 
 /* Location Classification */
 
-CREATE TABLE cre_location_type(
-    location_typeid BIGINT NOT NULL AUTO_INCREMENT,
+CREATE TABLE location_type(
+    location_type_id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     description TEXT,
-    PRIMARY KEY (location_typeid)
+    PRIMARY KEY (location_type_id)
 );
 
-CREATE TABLE cre_location(
-    locationid BIGINT NOT NULL AUTO_INCREMENT,
-    parentid BIGINT DEFAULT 0,
-    location_typeid BIGINT NOT NULL,
+
+
+CREATE TABLE location(
+    location_id BIGINT NOT NULL AUTO_INCREMENT,
+    parent_id BIGINT DEFAULT 0,
+    location_type_id BIGINT NOT NULL,
     name VARCHAR(100) NOT NULL,
     value VARCHAR(50), -- for dropdowns if needed
     decription TEXT,
-    PRIMARY KEY (locationid),
-    FOREIGN KEY (location_typeid) REFERENCES cre_location_type(location_typeid)
+    PRIMARY KEY (location_id),
+    FOREIGN KEY (location_type_id) REFERENCES location_type(location_type_id)
 );
+
 
 
 /******************/
@@ -147,4 +78,216 @@ CREATE TABLE devel_logsql (
 		  tracer text NOT NULL,
 		  timer decimal(16,6) NOT NULL
 );
+
+
+/* This is changed
+
+CREATE TABLE metadata (
+    meta_id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    caption VARCHAR(50),
+    type VARCHAR(50),
+    form_meta TEXT,
+    table_name VARCHAR(50),
+    validation_func VARCHAR(50),
+    PRIMARY KEY (meta_id)
+);
+
+CREATE TABLE module_metadata(
+    module_id BIGINT NOT NULL,
+    meta_id BIGINT NOT NULL,
+    section VARCHAR(50),
+    forms INT,
+    form_meta TEXT,
+    field_list TEXT,
+    element_order INT,
+    PRIMARY KEY(module_id,meta_id),
+    FOREIGN KEY (meta_id) REFERENCES metadata(meta_id),
+    FOREIGN KEY (module_id) REFERENCES modules(module_id)
+);
+
+CREATE TABLE people_reg(
+    rec_id BIGINT NOT NULL AUTO_INCREMENT,
+    form_id VARCHAR(100) NOT NULL,
+    meta_id BIGINT NOT NULL,
+    updated TIMESTAMP NOT NULL DEFAULT NOW(),
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    PRIMARY KEY (rec_id),
+    FOREIGN KEY (meta_id) REFERENCES metadata(meta_id)
+);
+
+CREATE TABLE metadata_int(
+    rec_id BIGINT NOT NULL,
+    value BIGINT,
+    PRIMARY KEY(rec_id),
+    FOREIGN KEY (rec_id) REFERENCES people_reg(rec_id)
+);
+
+CREATE TABLE metadata_text(
+    rec_id BIGINT NOT NULL,
+    value TEXT,
+    PRIMARY KEY(rec_id),
+    FOREIGN KEY (rec_id) REFERENCES people_reg(rec_id)
+);
+   
+CREATE TABLE metadata_date(
+    rec_id BIGINT NOT NULL,
+    value timestamp,
+    PRIMARY KEY(rec_id),
+    FOREIGN KEY (rec_id) REFERENCES  people_reg(rec_id)
+); 
+
+-- USERS
+CREATE TABLE users(
+    rec_id BIGINT NOT NULL,
+	username VARCHAR(10) NOT NULL,
+	password VARCHAR(40) NOT NULL,
+	PRIMARY KEY (rec_id),
+	FOREIGN KEY (rec_id) REFERENCES people_reg(rec_id)
+);
+
+COMMENT ON TABLE users IS 'User Information';
+
+CREATE TABLE dirty_tables(
+    tablename TEXT NOT NULL,
+    updated TIMESTAMP NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (tablename)
+);
+*/
+
+CREATE TABLE people_reg (
+    form_id BIGINT NOT NULL AUTO_INCREMENT,
+    fname VARCHAR(100),
+    lname VARCHAR(100),
+    oname TEXT,
+    address TEXT,
+    phone TEXT,
+    mobile TEXT,
+    fax TEXT,
+    page TEXT,
+    email TEXT,
+    PRIMARY KEY (form_id)
+);
+    
+CREATE TABLE people_working_details (
+    form_id BIGINT NOT NULL,
+    organization TEXT,
+    address TEXT,
+    phone TEXT,
+    fax TEXT,
+    email TEXT,
+    PRIMARY KEY (form_id),
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id)
+);
+
+CREATE TABLE people_identification (
+    form_id BIGINT NOT NULL,
+    ssn VARCHAR(100),
+    nic VARCHAR(100),
+    passport_no VARCHAR(100),
+    licence_no VARCHAR(100),
+    other TEXT,
+    PRIMARY KEY (form_id),
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id)
+);
+
+CREATE TABLE people_location (
+    form_id BIGINT NOT NULL,
+    location_id BIGINT NOT NULL,
+    PRIMARY KEY (form_id, location_id),
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id),
+    FOREIGN KEY (location_id) REFERENCES location(location_id)
+);
+
+CREATE TABLE people_extended (
+    form_id BIGINT NOT NULL,
+    race TEXT,
+    religion TEXT,
+    marital_status VARCHAR(1),
+    PRIMARY KEY (form_id),
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id)
+);
+
+CREATE TABLE dvr_group (
+    group_id BIGINT NOT NULL AUTO_INCREMENT,
+    group_name TEXT,
+    group_type TEXT,
+    PRIMARY KEY (group_id)
+);
+
+CREATE TABLE dvr_people_group (
+    form_id BIGINT NOT NULL,
+    group_id BIGINT NOT NULL,   
+    PRIMARY KEY (form_id, group_id),
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id),
+    FOREIGN KEY (group_id) REFERENCES dvr_group(group_id)
+);
+
+CREATE TABLE dvr_group_extended (
+    group_id BIGINT NOT NULL,
+    race TEXT,
+    ethnic_group TEXT,
+    other TEXT,
+    PRIMARY KEY (group_id),
+    FOREIGN KEY (group_id) REFERENCES dvr_group(group_id)
+);
+
+CREATE TABLE dvr_next_kin (
+    form_id BIGINT NOT NULL,
+    next_kin_id BIGINT NOT NULL,
+    relation TEXT,
+    PRIMARY KEY (form_id, next_kin_id),
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id),
+    FOREIGN KEY (next_kin_id) REFERENCES people_reg(form_id)
+);
+
+CREATE TABLE dvr_medical (
+    form_id BIGINT NOT NULL,
+    blood_type VARCHAR(10),
+    height VARCHAR(10),
+    weight VARCHAR(10),
+    eye_color VARCHAR(50),
+    skin_color VARCHAR(50),
+    hair_color VARCHAR(50),
+    
+    PRIMARY KEY (form_id) ,
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id)
+);
+
+CREATE TABLE dvr_missing (
+    form_id BIGINT NOT NULL,
+    last_seen TEXT,
+    last_clothing TEXT,
+    comments TEXT,
+    PRIMARY KEY (form_id) ,
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id)
+);
+
+CREATE TABLE dvr_deceased (
+    form_id BIGINT NOT NULL,
+    deceased TEXT,
+    date_of_death TIMESTAMP,
+    place_of_death TEXT,
+    comments TEXT,
+    PRIMARY KEY (form_id) ,
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id)
+);
+
+CREATE TABLE dvr_injured (
+    form_id BIGINT NOT NULL,
+    injury_type TEXT,
+    immediate_needs TEXT,
+    comments TEXT,
+    PRIMARY KEY (form_id) ,
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id)
+);
+
+CREATE TABLE dvr_displaced (
+    form_id BIGINT NOT NULL,
+    current_address TEXT,
+    previous_address TEXT,
+    PRIMARY KEY (form_id) ,
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id)
+);
+
 
--- sahana-phase2/inst/postgres.sql
+++ sahana-phase2/inst/postgres.sql
@@ -2,10 +2,9 @@
 set DateStyle to ISO;
 
 /* CORE SCHEMA */
-/* prefix : cre_ */
 
 -- SESSIONS
-CREATE TABLE cre_sessions(
+CREATE TABLE sessions(
 	sesskey VARCHAR(32) NOT NULL,
 	expiry INT NOT NULL,
 	expireref VARCHAR(64),
@@ -13,155 +12,73 @@ CREATE TABLE cre_sessions(
 	PRIMARY KEY (sesskey)
 );
 
-COMMENT ON TABLE cre_sessions IS 'Session Data';
+COMMENT ON TABLE sessions IS 'Session Data';
 
 
--- PEOPLE
-CREATE TABLE cre_people(
-	id BIGSERIAL NOT NULL,
-	firstname VARCHAR(40),
-	lastame VARCHAR(100),
-    othernames TEXT,
-    email VARCHAR(255),
-    address TEXT,
-	PRIMARY KEY (id)
-);
-
-COMMENT ON TABLE cre_people IS 'People List (will change later as the DVR comes in)';
-
--- USERS
-CREATE TABLE cre_users(
-    userid BIGINT NOT NULL,
-	username VARCHAR(10) NOT NULL,
-	password VARCHAR(40) NOT NULL,
-	PRIMARY KEY (userid),
-	FOREIGN KEY (userid) REFERENCES cre_people(id)
-);
-
-COMMENT ON TABLE cre_users IS 'User Information';
-
-/* CORE LOG SCHEMA */
-
-	-- USER LOGS
-	CREATE TABLE cre_userlog(
-		user_logid BIGSERIAL NOT NULL,
-		userid BIGINT NOT NULL,
-		ip inet NOT NULL,
-		logintime TIMESTAMP NOT NULL DEFAULT NOW(),
-		PRIMARY KEY(user_logid),
-		FOREIGN KEY (userid) REFERENCES cre_users (userid)
-	);
-	
-	COMMENT ON TABLE cre_userlog IS 'User login Log';
-	
-	-- ERROR LOG
-	CREATE TABLE cre_errorlog(
-		error_logid BIGSERIAL NOT NULL,
-		user_logid BIGINT NOT NULL,
-		severity VARCHAR(10) NOT NULL,
-		file TEXT  NOT NULL,
-		time TIMESTAMP NOT NULL DEFAULT NOW(),
-		message TEXT NOT NULL,
-		line INT NOT NULL,
-		PRIMARY KEY (error_logid),
-		FOREIGN KEY (user_logid) REFERENCES cre_userlog(user_logid)
-	);
-	
-	COMMENT ON TABLE cre_errorlog IS 'Loggs System Error logs';
-	
-	-- ACTION LOG 
-	CREATE TABLE cre_actionlog(
-	    action_logid BIGSERIAL NOT NULL,
-	    userid BIGINT NOT NULL,
-	    user_logid BIGINT NOT NULL,
-	    message TEXT NOT NULL,
-	    loglevel VARCHAR(10) NOT NULL,
-        module VARCHAR(50) NOT NULL,
-	    date timestamp NOT NULL DEFAULT NOW(),
-	    PRIMARY KEY (action_logid),
-	    FOREIGN KEY (user_logid) REFERENCES cre_userlog(user_logid)
-	);
-	
-	COMMENT ON TABLE cre_actionlog IS 'User Action Log';
-	
-	-- DEBUG LOG
-	CREATE TABLE cre_debuglog(
-	    debug_logid BIGSERIAL NOT NULL,
-	    userid BIGINT NOT NULL,
-	    user_logid BIGINT NOT NULL,
-	    action VARCHAR (100) NOT NULL,
-	    modulename VARCHAR(50) NOT NULL,
-	    date timestamp NOT NULL DEFAULT NOW(),
-	    data TEXT,
-	    PRIMARY KEY (debug_logid),
-	    FOREIGN KEY (user_logid) REFERENCES cre_userlog(user_logid)
-	);
-	
-	COMMENT ON TABLE cre_debuglog IS 'Debug User actions';
 	
 /**** CORE LOG SCHEMA END *****/
 
 -- MODULES
-CREATE TABLE cre_modules(
-	moduleid BIGSERIAL NOT NULL,
+CREATE TABLE modules(
+	module_id BIGSERIAL NOT NULL,
 	name VARCHAR(50) NOT NULL,
 	description TEXT,
 	version VARCHAR(10) NOT NULL,
 	active BOOL NOT NULL DEFAULT FALSE ,
-	PRIMARY KEY (moduleid)
+	PRIMARY KEY (module_id)
 );
 
-COMMENT ON TABLE cre_modules IS 'Module Information';
+COMMENT ON TABLE modules IS 'Module Information';
 
 --CUSTOM MODULE CONFIGURATIONS
-CREATE TABLE cre_config(
-	configid BIGSERIAL NOT NULL,
+CREATE TABLE config(
+	config_id BIGSERIAL NOT NULL,
 	config_group VARCHAR(100),
 	name VARCHAR(50) NOT NULL,
 	value TEXT,
 	description TEXT,
 	type VARCHAR(10),
-	moduleid BIGINT,
-	PRIMARY KEY(configid),
-	FOREIGN KEY (moduleid) REFERENCES cre_modules (moduleid)
+	module_id BIGINT,
+	PRIMARY KEY(config_id),
+	FOREIGN KEY (module_id) REFERENCES modules (module_id)
 );
 
-COMMENT ON TABLE cre_config IS 'Module Configurations';
+COMMENT ON TABLE config IS 'Module Configurations';
 
 --CUSTOM CONFIGURATION LISTS (SELECT)
-CREATE TABLE cre_configlist(
+CREATE TABLE configlist(
 	description TEXT NOT NULL,
 	value VARCHAR(50),
-	configid BIGINT NOT NULL,
-	FOREIGN KEY (configid) REFERENCES cre_config (configid)
+	config_id BIGINT NOT NULL,
+	FOREIGN KEY (config_id) REFERENCES config (config_id)
 );
 
-COMMENT ON TABLE cre_configlist IS 'Configuration List';
+COMMENT ON TABLE configlist IS 'Configuration List';
 
 /* Location Classification */
 
-CREATE TABLE cre_location_type(
-    location_typeid BIGSERIAL NOT NULL,
+CREATE TABLE location_type(
+    location_type_id BIGSERIAL NOT NULL,
     name VARCHAR(100) NOT NULL,
     description TEXT,
-    PRIMARY KEY (location_typeid)
+    PRIMARY KEY (location_type_id)
 );
 
-COMMENT ON TABLE cre_location_type IS 'Types of Locations';
+COMMENT ON TABLE location_type IS 'Types of Locations';
 
 
-CREATE TABLE cre_location(
-    locationid BIGSERIAL NOT NULL,
-    parentid BIGINT DEFAULT 0,
-    location_typeid BIGINT NOT NULL,
+CREATE TABLE location(
+    location_id BIGSERIAL NOT NULL,
+    parent_id BIGINT DEFAULT 0,
+    location_type_id BIGINT NOT NULL,
     name VARCHAR(100) NOT NULL,
     value VARCHAR(50), -- for dropdowns if needed
     decription TEXT,
-    PRIMARY KEY (locationid),
-    FOREIGN KEY (location_typeid) REFERENCES cre_location_type(location_typeid)
+    PRIMARY KEY (location_id),
+    FOREIGN KEY (location_type_id) REFERENCES location_type(location_type_id)
 );
 
-COMMENT ON TABLE cre_location IS 'Location Classification Table';
+COMMENT ON TABLE location IS 'Location Classification Table';
 
 
 /******************/
@@ -177,4 +94,215 @@ CREATE TABLE devel_logsql (
 );
 
 COMMENT ON TABLE devel_logsql IS 'log sql for optimization';
+
+/* This is changed
+
+CREATE TABLE metadata (
+    meta_id BIGSERIAL NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    caption VARCHAR(50),
+    type VARCHAR(50),
+    form_meta TEXT,
+    table_name VARCHAR(50),
+    validation_func VARCHAR(50),
+    PRIMARY KEY (meta_id)
+);
+
+CREATE TABLE module_metadata(
+    module_id BIGINT NOT NULL,
+    meta_id BIGINT NOT NULL,
+    section VARCHAR(50),
+    forms INT,
+    form_meta TEXT,
+    field_list TEXT,
+    element_order INT,
+    PRIMARY KEY(module_id,meta_id),
+    FOREIGN KEY (meta_id) REFERENCES metadata(meta_id),
+    FOREIGN KEY (module_id) REFERENCES modules(module_id)
+);
+
+CREATE TABLE people_reg(
+    rec_id BIGSERIAL NOT NULL,
+    form_id VARCHAR(100) NOT NULL,
+    meta_id BIGINT NOT NULL,
+    updated TIMESTAMP NOT NULL DEFAULT NOW(),
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    PRIMARY KEY (rec_id),
+    FOREIGN KEY (meta_id) REFERENCES metadata(meta_id)
+);
+
+CREATE TABLE metadata_int(
+    rec_id BIGINT NOT NULL,
+    value BIGINT,
+    PRIMARY KEY(rec_id),
+    FOREIGN KEY (rec_id) REFERENCES people_reg(rec_id)
+);
+
+CREATE TABLE metadata_text(
+    rec_id BIGINT NOT NULL,
+    value TEXT,
+    PRIMARY KEY(rec_id),
+    FOREIGN KEY (rec_id) REFERENCES people_reg(rec_id)
+);
+   
+CREATE TABLE metadata_date(
+    rec_id BIGINT NOT NULL,
+    value timestamp,
+    PRIMARY KEY(rec_id),
+    FOREIGN KEY (rec_id) REFERENCES  people_reg(rec_id)
+); 
+
+-- USERS
+CREATE TABLE users(
+    rec_id BIGINT NOT NULL,
+	username VARCHAR(10) NOT NULL,
+	password VARCHAR(40) NOT NULL,
+	PRIMARY KEY (rec_id),
+	FOREIGN KEY (rec_id) REFERENCES people_reg(rec_id)
+);
+
+COMMENT ON TABLE users IS 'User Information';
+
+CREATE TABLE dirty_tables(
+    tablename TEXT NOT NULL,
+    updated TIMESTAMP NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (tablename)
+);
+*/
+
+CREATE TABLE people_reg (
+    form_id BIGSERIAL NOT NULL,
+    fname VARCHAR(100),
+    lname VARCHAR(100),
+    oname TEXT,
+    address TEXT,
+    phone TEXT,
+    mobile TEXT,
+    fax TEXT,
+    page TEXT,
+    email TEXT,
+    PRIMARY KEY (form_id)
+);
+    
+CREATE TABLE people_working_details (
+    form_id BIGINT NOT NULL,
+    organization TEXT,
+    address TEXT,
+    phone TEXT,
+    fax TEXT,
+    email TEXT,
+    PRIMARY KEY (form_id),
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id)
+);
+
+CREATE TABLE people_identification (
+    form_id BIGINT NOT NULL,
+    ssn VARCHAR(100),
+    nic VARCHAR(100),
+    passport_no VARCHAR(100),
+    licence_no VARCHAR(100),
+    other TEXT,
+    PRIMARY KEY (form_id),
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id)
+);
+
+CREATE TABLE people_location (
+    form_id BIGINT NOT NULL,
+    location_id BIGINT NOT NULL,
+    PRIMARY KEY (form_id, location_id),
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id),
+    FOREIGN KEY (location_id) REFERENCES location(location_id)
+);
+
+CREATE TABLE people_extended (
+    form_id BIGINT NOT NULL,
+    race TEXT,
+    religion TEXT,
+    marital_status VARCHAR(1),
+    PRIMARY KEY (form_id),
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id)
+);
+
+CREATE TABLE dvr_group (
+    group_id BIGSERIAL NOT NULL,
+    group_name TEXT,
+    group_type TEXT,
+    PRIMARY KEY (group_id)
+);
+
+CREATE TABLE dvr_people_group (
+    form_id BIGINT NOT NULL,
+    group_id BIGINT NOT NULL,   
+    PRIMARY KEY (form_id, group_id),
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id),
+    FOREIGN KEY (group_id) REFERENCES dvr_group(group_id)
+);
+
+CREATE TABLE dvr_group_extended (
+    group_id BIGINT NOT NULL,
+    race TEXT,
+    ethnic_group TEXT,
+    other TEXT,
+    PRIMARY KEY (group_id),
+    FOREIGN KEY (group_id) REFERENCES dvr_group(group_id)
+);
+
+CREATE TABLE dvr_next_kin (
+    form_id BIGINT NOT NULL,
+    next_kin_id BIGINT NOT NULL,
+    relation TEXT,
+    PRIMARY KEY (form_id, next_kin_id),
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id),
+    FOREIGN KEY (next_kin_id) REFERENCES people_reg(form_id)
+);
+
+CREATE TABLE dvr_medical (
+    form_id BIGINT NOT NULL,
+    blood_type VARCHAR(10),
+    height VARCHAR(10),
+    weight VARCHAR(10),
+    eye_color VARCHAR(50),
+    skin_color VARCHAR(50),
+    hair_color VARCHAR(50),
+    
+    PRIMARY KEY (form_id) ,
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id)
+);
+
+CREATE TABLE dvr_missing (
+    form_id BIGINT NOT NULL,
+    last_seen TEXT,
+    last_clothing TEXT,
+    comments TEXT,
+    PRIMARY KEY (form_id) ,
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id)
+);
+
+CREATE TABLE dvr_deceased (
+    form_id BIGINT NOT NULL,
+    deceased TEXT,
+    date_of_death TIMESTAMP,
+    place_of_death TEXT,
+    comments TEXT,
+    PRIMARY KEY (form_id) ,
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id)
+);
+
+CREATE TABLE dvr_injured (
+    form_id BIGINT NOT NULL,
+    injury_type TEXT,
+    immediate_needs TEXT,
+    comments TEXT,
+    PRIMARY KEY (form_id) ,
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id)
+);
+
+CREATE TABLE dvr_displaced (
+    form_id BIGINT NOT NULL,
+    current_address TEXT,
+    previous_address TEXT,
+    PRIMARY KEY (form_id) ,
+    FOREIGN KEY (form_id) REFERENCES people_reg(form_id)
+);
+
 
