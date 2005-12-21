fa268f8e97f60c6fd73f8563e690ae1ece7188c5
Sudheera Fernando <>
2005-12-21 13:41:21 +0000

    New rms files

--- sahana-phase2/mod/rms/ins/rms.sql
+++ sahana-phase2/mod/rms/ins/rms.sql
@@ -6,87 +6,123 @@ USE sahana;
 
 DROP TABLE IF EXISTS `rms_request`;
 CREATE TABLE rms_request (
-    req_id BIGINT NOT NULL AUTO_INCREMENT,
-    req_date TIMESTAMP,
-    name VARCHAR(100),
-    contact VARCHAR(100),
-    address VARCHAR(255),
-    site_name VARCHAR(100),
-    site_district VARCHAR(100),
-    site_address VARCHAR(255),
-    comments VARCHAR(500),
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    date TIMESTAMP,
+    site_id BIGINT,
     status VARCHAR(100) DEFAULT 'open',
-    user_id BIGINT,
-    PRIMARY KEY (req_id),
-    FOREIGN KEY (user_id) REFERENCES org_user (user_id)
+    requester_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (requester_id) REFERENCES rms_org_psn (id),
+    FOREIGN KEY (site_id) REFERENCES rms_req_site (id)
 );
 
-DROP TABLE IF EXISTS `rms_req_category`;
-CREATE TABLE rms_req_category (
-    cat_id BIGINT NOT NULL AUTO_INCREMENT,
-    category VARCHAR(100) NOT NULL,
-    description VARCHAR(255),
-    PRIMARY KEY (cat_id)
+DROP TABLE IF EXISTS `rms_org_psn`;
+CREATE TABLE rms_org_psn (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    location VARCHAR(255),
+    telephone VARCHAR(255),
+    email VARCHAR(255),
+    address VARCHAR(255),
+    comments VARCHAR(255),
+    PRIMARY KEY (id)
 );
-/**
-*Intial category type data (Should be localized??)
-*/
-INSERT INTO rms_req_category (category, description) values ('Blankets Shelter','later');
-INSERT INTO rms_req_category (category, description) values ('Medical Drugs','later');
-INSERT INTO rms_req_category (category, description) values ('Food and Nutrition','later');
-INSERT INTO rms_req_category (category, description) values ('Other','Other Categories');
 
-
-DROP TABLE IF EXISTS `rms_req_priority`;
-CREATE TABLE rms_req_priority (
-    priority_id BIGINT NOT NULL AUTO_INCREMENT,
-    priority VARCHAR(100) NOT NULL,
-    description VARCHAR(255),
-    PRIMARY KEY (priority_id)
+DROP TABLE IF EXISTS `rms_req_site`;
+CREATE TABLE rms_req_site (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    address VARCHAR(255),
+    location VARCHAR(255),
+    PRIMARY KEY (id)
 );
-/**
-*Intial priority type data (Should be localized??)
-*/
-INSERT INTO rms_req_priority (priority,description) values ('Immediate (< 1 week)','sum desc');
-INSERT INTO rms_req_priority (priority,description) values ('Medium (< 1 mon)','sum desc');
-INSERT INTO rms_req_priority (priority,description) values ('Long Term (1-3 mon)','sum desc');
 
+DROP TABLE IF EXISTS `rms_item`;
+CREATE TABLE rms_item (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    cat_id BIGINT,
+    unit VARCHAR(100),
+    qty BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (cat_id) REFERENCES rms_item_cat (id)
+);
 
 DROP TABLE IF EXISTS `rms_req_item`;
 CREATE TABLE rms_req_item (
-    req_item_id BIGINT NOT NULL AUTO_INCREMENT,
-    cat_id BIGINT,
-    item VARCHAR(200),
-    units VARCHAR(100),
-    quantity INT,
-    priority_id BIGINT,
     req_id BIGINT NOT NULL,
-    PRIMARY KEY (req_item_id),
-    FOREIGN KEY (req_id) REFERENCES rms_request(req_id),
-    FOREIGN KEY (cat_id) REFERENCES rms_req_category(cat_id),
-    FOREIGN KEY (priority_id) REFERENCES rms_req_priority(priority_id)
+    item_id BIGINT NOT NULL,
+    priority_id BIGINT,
+    FOREIGN KEY (priority_id) REFERENCES rms_item_pry (id)
 );
 
-DROP TABLE IF EXISTS `rms_req_ff`;
-CREATE TABLE rms_req_ff (
-    req_ff_id BIGINT NOT NULL AUTO_INCREMENT,
-    req_item_id BIGINT,
-    user_id BIGINT,
-    quantity INT,
-    ff_status VARCHAR(100),
-    req_donor_id BIGINT,
-    ff_date TIMESTAMP,
-    PRIMARY KEY (req_ff_id),
-    FOREIGN KEY (req_donor_id) REFERENCES rms_req_donor(req_donor_id),
-    FOREIGN KEY (req_item_id) REFERENCES rms_req_item(req_item_id),
-    FOREIGN KEY (user_id) REFERENCES users (puu_id)
+DROP TABLE IF EXISTS `rms_item_pry`;
+CREATE TABLE rms_item_pry (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    priority VARCHAR(255),
+    description VARCHAR(255),
+    PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS `rms_req_donor`;
-CREATE TABLE rms_req_donor (
-    req_donor_id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255),
-    contact VARCHAR(255),
-    comments VARCHAR(255),
-    PRIMARY KEY (req_donor_id)
+/**
+*Intial priority type data
+*/
+INSERT INTO rms_item_pry (priority,description) values ('Immediate (< 1 week)','desc');
+INSERT INTO rms_item_pry (priority,description) values ('Medium (< 1 mon)','desc');
+INSERT INTO rms_item_pry (priority,description) values ('Long Term (1-3 mon)','desc');
+
+
+DROP TABLE IF EXISTS `rms_item_cat`;
+CREATE TABLE rms_item_cat (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    category VARCHAR(255),
+    description VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+/**
+*Intial category type data
+*/
+INSERT INTO rms_item_cat (category, description) values ('Blankets Shelter','later');
+INSERT INTO rms_item_cat (category, description) values ('Medical Drugs','later');
+INSERT INTO rms_item_cat (category, description) values ('Food and Nutrition','later');
+INSERT INTO rms_item_cat (category, description) values ('Other','Other Categories');
+
+DROP TABLE IF EXISTS `rms_req_stat`;
+CREATE TABLE rms_req_stat (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    status VARCHAR(255),
+    description VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS `rms_ff_item`;
+CREATE TABLE rms_ff_item (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    req_id BIGINT,
+    item_id BIGINT,
+    date TIMESTAMP,
+    qty BIGINT,
+    status VARCHAR(255),
+    donor_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (donor_id) REFERENCES rms_org_psn (id)
+);
+
+DROP TABLE IF EXISTS `rms_offer`;
+CREATE TABLE rms_offer (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    date TIMESTAMP,
+    donor_id BIGINT,
+    status VARCHAR(255),
+    PRIMARY KEY (id),
+    FOREIGN KEY (donor_id) REFERENCES rms_org_psn (id)
+);
+
+DROP TABLE IF EXISTS `rms_offer_item`;
+CREATE TABLE rms_offer_item (
+    offer_id BIGINT NOT NULL,
+    item_id BIGINT NOT NULL,
+    FOREIGN KEY (offer_id) REFERENCES rms_offer (id),
+    FOREIGN KEY (item_id) REFERENCES rms_item (id)
 );
