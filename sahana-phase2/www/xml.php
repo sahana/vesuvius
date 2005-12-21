2c0c39b3ffbb93bebe24a2d476a09387b55b74d2
ravindra <ravindra@opensource.lk>
2005-12-21 12:04:14 +0000

    minor changes

--- sahana-phase2/www/xml.php
+++ sahana-phase2/www/xml.php
@@ -13,10 +13,19 @@
 * @copyright  Lanka Software Foundation - http://www.opensource.lk
 *
 */
+
+/* }}} */
+$act=$_GET{"act"};
+if($act=='add_loc'){
+    _shn_get_level_location();
+}else {
+    _shn_get_children();
+}
+
+
+function _shn_get_children(){
 require_once('../3rd/adodb/adodb.inc.php');
 require_once('../conf/config.inc');
-/* }}} */
-
 //Make the connection to $global['db']
 $db = NewADOConnection($conf['db_engine']);
 $db ->Connect($conf['db_host'].($conf['db_port']?':'.$conf['db_por
@@ -34,5 +43,31 @@ $q = "select location.name,location.location_id,parent_id from location where lo
         $res_child->MoveNext();
     }
 echo $res;
+}
+
+function _shn_get_level_location(){
+require_once('../3rd/adodb/adodb.inc.php');
+require_once('../conf/config.inc');
+//Make the connection to $global['db']
+$db = NewADOConnection($conf['db_engine']);
+$db ->Connect($conf['db_host'].($conf['db_port']?':'.$conf['db_por
+t']:''),$conf['db_user'],$conf['db_pass'],$conf['db_name']);
+
+
+$level=$_GET{"sel"};
+if($level==1){
+echo "none,";
+}
+$q = "select location.name,location.location_id,parent_id from location where location.opt_location_type={$level}";
+    $res_child=$db->Execute($q);
+    if($res_child->EOF)
+        return;
+    while(!$res_child->EOF){
+        $res=$res.",".$res_child->fields[1];
+        $res=$res.",".$res_child->fields[0];
+        $res_child->MoveNext();
+    }
+echo $res;
+}
 ?>
 
