ecdcb630e79f3ab9d704d6961d3034e3e1de7b31
Mifan Careem <mifanc@gmail.com>
2005-12-21 04:10:42 +0000

    loads of additions ;) gis module has its mini mechanism to handle new modules dynamically

---
+++ sahana-phase2/mod/gis/dbcreate.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS sahana;
+USE sahana;
+
+/* Geographic Information System and Mapping Tables
+----------------------------------------------------
+*/
+
+DROP TABLE IF EXISTS `gis_location`;
+CREATE TABLE `gis_location` (
+  `poc_uuid` bigint(20) NOT NULL,
+	`location_id` varchar(20) default NULL,
+	`opt_gis_mod` varchar(30) default NULL,
+	`map_northing` double(15,10) NOT NULL,
+	`map_easting` double(15,10) NOT NULL,
+	`map_projection` varchar(20) default NULL,
+	`opt_gis_marker` varchar(20) default NULL,
+	`gis_uid` bigint(20) NOT NULL,
+	PRIMARY KEY  (`gis_uid`)
+);
+									
