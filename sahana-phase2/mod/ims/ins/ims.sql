fbe1d0125adb01aea6a5a7b51b09df68f3aab9fe
mahesh <>
2006-05-03 12:47:07 +0000

    *** empty log message ***

--- sahana-phase2/mod/ims/ins/ims.sql
+++ sahana-phase2/mod/ims/ins/ims.sql
@@ -7,19 +7,15 @@ CREATE TABLE ims_item_records
 
 	item_id BIGINT NOT NULL AUTO_INCREMENT,
 	catalog_id VARCHAR(100),
-	category VARCHAR(20),
 	inv_id VARCHAR(60),
+	transit_id BIGINT,
 	item_name VARCHAR(100),
 	amount BIGINT,
 	unit VARCHAR(20),
 	manufactured_date DATE,
 	expire_date DATE,
+	cost_per_unit VARCHAR(100),
 	state VARCHAR(20),
-	transit_state VARCHAR(20),
-	item_send_to VARCHAR(20),
-	item_send_from VARCHAR(20),
-	amount_send BIGINT,
-	amount_received BIGINT,
 	PRIMARY KEY(item_id)
 );
 
@@ -37,6 +33,24 @@ CREATE TABLE ims_inventory_records
 	resources TEXT,
 	space VARCHAR(100),
 	PRIMARY KEY(inv_uuid)	
+);
+
+DROP TABLE IF EXISTS `ims_transfer_item`;
+CREATE TABLE ims_transfer_item
+(
+	transit_id BIGINT NOT NULL AUTO_INCREMENT,
+	item_id BIGINT,
+	amount_send VARCHAR(20),
+	inv_id_to BIGINT,
+	person_send VARCHAR(100),
+	date_send DATE,
+	distribution_method VARCHAR(100),
+	received_item_id BIGINT,
+	amount_received VARCHAR(20),
+	person_received VARCHAR(20),
+	date_received DATE,
+	PRIMARY KEY(transit_id)
+	
 );
 
 
