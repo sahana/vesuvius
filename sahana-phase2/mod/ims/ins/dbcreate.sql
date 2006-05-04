91a7829b40897ed935e3a30166f719f2fc26d41d
Chamindra de Silva <chamindra@opensource.lk>
2006-05-04 12:37:16 +0000

    renamed ims.sql to dbcreate.sql

---
+++ sahana-phase2/mod/ims/ins/dbcreate.sql
@@ -0,0 +1,56 @@
+USE sahana;
+/* INVENTORY MANAGEMENT SYSTEM TABLES */
+/* --------------------------------------------------------------------------*/
+DROP TABLE IF EXISTS `ims_item_records`;
+CREATE TABLE ims_item_records
+(
+
+	item_id BIGINT NOT NULL AUTO_INCREMENT,
+	catalog_id VARCHAR(100),
+	inv_id VARCHAR(60),
+	transit_id BIGINT,
+	item_name VARCHAR(100),
+	amount BIGINT,
+	unit VARCHAR(20),
+	manufactured_date DATE,
+	expire_date DATE,
+	cost_per_unit VARCHAR(100),
+	state VARCHAR(20),
+	PRIMARY KEY(item_id)
+);
+
+DROP TABLE IF EXISTS `ims_inventory_records`;
+CREATE TABLE ims_inventory_records
+(
+
+	inv_uuid BIGINT NOT NULL AUTO_INCREMENT,
+	parent_id BIGINT,
+	inventory_name VARCHAR(100),
+	inventory_type VARCHAR(100),
+	reg_no VARCHAR(100),
+	man_power VARCHAR(100),
+	equipment VARCHAR(100),
+	resources TEXT,
+	space VARCHAR(100),
+	PRIMARY KEY(inv_uuid)	
+);
+
+DROP TABLE IF EXISTS `ims_transfer_item`;
+CREATE TABLE ims_transfer_item
+(
+	transit_id BIGINT NOT NULL AUTO_INCREMENT,
+	item_id BIGINT,
+	amount_send VARCHAR(20),
+	inv_id_to BIGINT,
+	person_send VARCHAR(100),
+	date_send DATE,
+	distribution_method VARCHAR(100),
+	received_item_id BIGINT,
+	amount_received VARCHAR(20),
+	person_received VARCHAR(20),
+	date_received DATE,
+	PRIMARY KEY(transit_id)
+	
+);
+
+
