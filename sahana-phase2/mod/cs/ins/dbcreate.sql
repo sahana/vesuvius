ba58df232ca59ee497942fb7de264ddbdaf9fcc2
jayasinghe <>
2006-05-29 10:08:21 +0000

    ct_catalogue.sql removed

--- sahana-phase2/mod/cs/ins/dbcreate.sql
+++ sahana-phase2/mod/cs/ins/dbcreate.sql
@@ -5,9 +5,30 @@ USE sahana;
 DROP TABLE IF EXISTS `ct_catalogue`;
 DROP TABLE IF EXISTS `ct_unit`;
 DROP TABLE IF EXISTS `ct_cat_unit`;
-create table ct_catalogue (ct_uuid varchar(60) not null primary key, parentid varchar(60),name varchar(100),description varchar(100));
+DROP TABLE IF EXISTS `ct_unit_type`;
+CREATE TABLE ct_catalogue 
+	(ct_uuid varchar(60) NOT NULL PRIMARY KEY, 
+	parentid varchar(60),
+	name varchar(100),
+	description varchar(100),
+	final_flag varchar(1) DEFAULT '0');
 
+CREATE TABLE ct_unit
+	(unit_type_uuid varchar(60),
+	unit_uuid varchar(60) NOT NULL PRIMARY KEY,
+	name varchar(100),
+	base_flag varchar(1) DEFAULT '0',
+	multiplier DOUBLE);
 
-create table ct_unit(unit_uuid varchar(60) not null primary key,name varchar(100));
+CREATE TABLE ct_cat_unit
+	(ct_uuid varchar(60),
+	unit_uuid varchar(60));
 
-create table ct_cat_unit(ct_uuid varchar(60),unit_uuid varchar(60));
+CREATE TABLE ct_unit_type
+	(unit_type_uuid varchar(60) NOT NULL PRIMARY KEY,
+	name varchar(100),
+	description varchar(100));
+
+INSERT INTO field_options
+	(field_name,option_code,option_description) 
+	VALUES('opt_cs_depth','1','Maximum Depth of catalogue / Items');
