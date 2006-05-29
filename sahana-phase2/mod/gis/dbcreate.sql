cebf51cad5efcb280fea1d37f7ab3031ccfb0095
Mifan Careem <mifanc@gmail.com>
2006-05-29 07:42:39 +0000

    added gis api functionality

--- sahana-phase2/mod/gis/dbcreate.sql
+++ sahana-phase2/mod/gis/dbcreate.sql
@@ -4,21 +4,21 @@
 
 DROP TABLE IF EXISTS `gis_location`;
 CREATE TABLE `gis_location` (
-  `poc_uuid` bigint(20) NOT NULL,
+  `poc_uuid` varchar(60) NOT NULL,
 	`location_id` varchar(20) default NULL,
 	`opt_gis_mod` varchar(30) default NULL,
 	`map_northing` double(15,10) NOT NULL,
 	`map_easting` double(15,10) NOT NULL,
 	`map_projection` varchar(20) default NULL,
 	`opt_gis_marker` varchar(20) default NULL,
-	`gis_uid` bigint(20) NOT NULL,
+	`gis_uid` varchar(60) NOT NULL,
 	PRIMARY KEY  (`gis_uid`)
 );
 
-/* --config options --*/
+/* --config options --
 
 INSERT INTO `config` ( `module_id` , `confkey` , `value` ) VALUES ('gis', 'google_key', '');
 INSERT INTO `config` ( `module_id` , `confkey` , `value` ) VALUES ('gis', 'center_x', '');
 INSERT INTO `config` ( `module_id` , `confkey` , `value` ) VALUES ('gis', 'center_y', '');
 INSERT INTO `config` ( `module_id` , `confkey` , `value` ) VALUES ('gis', 'type', '');
-
+*/
