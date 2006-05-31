05f67efed7189d943498867b3888c9bce6f3e093
Joseph Priyanga Fonseka <hevayo@gmail.com>
2006-05-31 05:45:03 +0000

    cookie for collapsible menu

--- sahana-phase2/www/theme/default/sahana.js
+++ sahana-phase2/www/theme/default/sahana.js
@@ -1,23 +1,80 @@
-window.onload=hide;
 
 //function used to expand or collaps the mod_sub_menu.
-function expand(id){
+function expand(id,el){
 	var menu=document.getElementById(id).style;
-	if(menu.display=='none')
+	if(menu.display=='block')
 	{
-		menu.display='block';	
+         el.className='smopen';
+		menu.display='none';
+        removeMenu(id);
 	} 
 	else{
-		menu.display='none';	
+        el.className='smclose';
+		menu.display='block';
+        addMenu(id);	
 	}
 }
 
-//function used to hide mod_sub_menu when the page loads. 
-function hide(){
-	el=document.getElementById("modmenu");
-	var child=el.getElementsByTagName("ul");
-	for (var i=0;i<child.length;i++){
-	child[i].style.display='none';
-	}	
+function changeClass(el){
+    var a=el.parentNode.getElementsByTagName('a');
+    a[0].className='smclose';
+}
+//function used to show the mod sub menus in startup
+function showMem(){
+    var list=getMenuList();
+    var el;
+    for (var i=0;i<list.length;i++){
+        if(list[i]=='')continue;
+    //        alert(list[i]);
+        el=document.getElementById(list[i]);
+        if(el)
+        {
+            //el.className='smclose';
+            changeClass(el);
+            el.style.display='block';
+        }
+        else
+            removeMenu(list[i]);
+    }
+}
+
+function addMenu(id)
+{
+    var list=getMenuList();
+    var st='';
+    for (var i=0;i<list.length;i++){
+        if(list[i]==id||list[i]=='')continue;
+        st=st+list[i]+"|";
+    }
+    st=st+id;
+    //alert(st);
+    document.cookie = "menulist="+st+" ; path=/";    
+}
+
+function removeMenu(id)
+{
+    var list=getMenuList();
+    var st='';
+    for (var i=0;i<list.length;i++){
+        if(list[i]==id||list[i]=='')continue;
+        st=st+list[i]+"|";
+    }
+    //alert(st);
+    document.cookie = "menulist="+st+" ; path=/";
+}
+
+function getMenuList()
+{
+    var nameEQ = "menulist=";
+    var namelist="";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++)
+    {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) 
+        namelist=c.substring(nameEQ.length,c.length);
+    }
+    return namelist.split('|');
 }
 
