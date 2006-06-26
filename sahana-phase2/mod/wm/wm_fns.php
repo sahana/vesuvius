b2c3b7d8227c2adf99956bfacf574eb3f75c9569
Mifan Careem <mifanc@gmail.com>
2006-06-26 05:47:42 +0000

    add functionlity for wikimaps

--- sahana-phase2/mod/wm/wm_fns.php
+++ sahana-phase2/mod/wm/wm_fns.php
@@ -26,4 +26,36 @@ function add_wiki_detail()
 {
 	
 }
+
+function show_wiki_add_detail($errors=false)
+{
+	if($errors){
+		echo "Errors";
+	}
+	global $global;
+	include_once $global['approot']."/inc/lib_form.inc";
+?>
+	<h2><?=_("Add Wiki Detail")?></h2>
+<?php
+	$type_help="yo";
+	$url_help="type url";
+	$date_help="type date";
+	
+	shn_form_fopen(awk);
+	shn_form_fsopen(_("Main Details"));
+	shn_form_text(_("Name of Detail"),"wiki_name",'size="50"',array('req'=>true,'help'=>$type_help));
+	shn_form_opt_select("opt_wikimap_type",_("Wiki Type"),array('help'=>$type_help));
+	shn_form_textarea(_("Detail Summary"),"wiki_text",'size="50"');
+	shn_form_fsclose();
+	shn_form_fsopen(_("Extra Details"));
+	shn_form_text(_("URL"),"wiki_url",'size="50"',array('help'=>$url_help));
+	shn_form_text(_("Date of Event"),"wiki_evnt_date",'size="50"',array('help'=>$date_help));
+	shn_form_fsclose();
+	shn_form_fsopen(_("Wikimap Options"));
+	shn_form_text(_("Author"),"wiki_author",'size="50"',array('help'=>$type_help));
+	shn_form_checkbox(_("Publicly Editable"),"edit_public");
+	shn_form_fsclose();
+	shn_form_submit(_("Add Detail"));
+	shn_form_fclose();
+}
 ?>
