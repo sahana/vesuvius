b7983342967a8bab3963b28da624b1117d03bddf
Mifan Careem <mifanc@gmail.com>
2006-07-03 07:22:53 +0000

    Changed Name

--- sahana-phase2/mod/wm/dbcreate.sql
+++ sahana-phase2/mod/wm/dbcreate.sql
@@ -11,11 +11,22 @@ CREATE TABLE `gis_wiki` (
 	`gis_uuid` VARCHAR(60) NOT NULL,
 	`name` varchar(50) NOT NULL,
 	`description` VARCHAR(100) default NULL,
+	`opt_category` VARCHAR(10),
 	`url` varchar(50) NOT NULL,
-	`event_date` timestamp default NULL,
-	`placement_date` timestamp NOT NULL,
-	`editable` boolean NOT NULL default 'false',
+	`event_date` timestamp NULL,
+	`placement_date` timestamp NOT NULL default now(),
+	`editable` boolean NOT NULL default false,
 	`author` VARCHAR(30) default NULL,
-	PRIMARY KEY  (`wiki_uuid`)
+	`approved` boolean default false,
+	PRIMARY KEY  (`wiki_uuid`),
+	FOREIGN KEY (gis_uuid) REFERENCES gis_location(gis_uid)
 );
 
+/*
+ * Sample Data for WIKIMap Types
+*/
+INSERT INTO field_options (field_name,option_code,option_description) VALUES ('opt_wikimap_type','gen','General');
+INSERT INTO field_options (field_name,option_code,option_description) VALUES ('opt_wikimap_type','per','Person Status');
+INSERT INTO field_options (field_name,option_code,option_description) VALUES ('opt_wikimap_type','dam','Damage Status');
+INSERT INTO field_options (field_name,option_code,option_description) VALUES ('opt_wikimap_type','dis','Disaster Status');
+INSERT INTO field_options (field_name,option_code,option_description) VALUES ('opt_wikimap_type','sos','Help Needed');	
