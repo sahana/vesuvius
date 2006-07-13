34fe61e69165ba78a0e6008c3c46e94da59c873b
Mifan Careem <mifanc@gmail.com>
2006-07-13 05:33:46 +0000

    Added wikimaps schema

--- sahana-phase2/mod/gis/dbcreate.sql
+++ sahana-phase2/mod/gis/dbcreate.sql
@@ -1,4 +1,4 @@
-/*$id$;*/
+/*$id$*/
 /* 
 *Landmark Location Registry Tables and sample-data 
 *@author: mifan@opensource.lk
@@ -48,4 +48,36 @@ CREATE TABLE `gis_location` (
 INSERT INTO `config` ( `module_id` , `confkey` , `value` ) VALUES ('gis', 'google_key', '');
 INSERT INTO `config` ( `module_id` , `confkey` , `value` ) VALUES ('gis', 'center_x', '79.5');
 INSERT INTO `config` ( `module_id` , `confkey` , `value` ) VALUES ('gis', 'center_y', '8.5');
+
+/*
+*GIS-WikiMaps Schema
+*@author: mifan@opensource.lk
+*----------------------------------------------------
+*/
+
+DROP TABLE IF EXISTS `gis_wiki`;
+CREATE TABLE `gis_wiki` (
+    `wiki_uuid` VARCHAR(60) NOT NULL,
+    `gis_uuid` VARCHAR(60) NOT NULL,
+    `name` varchar(50) NOT NULL,
+    `description` VARCHAR(100) default NULL,
+    `opt_category` VARCHAR(10),
+    `url` varchar(50) NOT NULL,
+    `event_date` timestamp NULL,
+    `placement_date` timestamp NOT NULL default now(),
+    `editable` boolean NOT NULL default false,
+    `author` VARCHAR(30) default NULL,
+    `approved` boolean default false,
+    PRIMARY KEY  (`wiki_uuid`),
+    FOREIGN KEY (gis_uuid) REFERENCES gis_location(gis_uid)
+);
+
+/*
+ * Sample Data for WIKIMap Types
+*/
+INSERT INTO field_options (field_name,option_code,option_description) VALUES ('opt_wikimap_type','gen','General');
+INSERT INTO field_options (field_name,option_code,option_description) VALUES ('opt_wikimap_type','per','Person Status');
+INSERT INTO field_options (field_name,option_code,option_description) VALUES ('opt_wikimap_type','dam','Damage Status');
+INSERT INTO field_options (field_name,option_code,option_description) VALUES ('opt_wikimap_type','dis','Disaster Status');
+INSERT INTO field_options (field_name,option_code,option_description) VALUES ('opt_wikimap_type','sos','Help Needed');
 
