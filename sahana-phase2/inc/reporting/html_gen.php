34117f774185020723dc93dfeb683225d1bcc818
jayasinghe <>
2006-07-21 06:24:02 +0000

    report ID generation added

--- sahana-phase2/inc/reporting/html_gen.php
+++ sahana-phase2/inc/reporting/html_gen.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Library for Generating xhtml
+ * Library for Generating xhtml Report
  *
  * PHP version 4 and 5
  *
@@ -17,15 +17,28 @@ class genhtml
 
 	var $file_name;
 	var $title_pos;
+	var $title_txt;
+
+	var $summary_pos;	
+	var $table_pos;
+	var $image_pos;	
+
 	var $output_code;
 	var $background_color;
+	var $file_format;
 
+	var $keyword;
+	var $report_owner;
+	var $extention;
+	var $report_id;
 	/*
 	* The constructor 
 	*/
 	function genhtml()
 		{
 		$this->output_code .="<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n<html>\n<head><title>Sahana Report</title></head>\n<body>\n";
+		$this->file_format = "xhtml";
+		$this->extention = ".html";
 		}	
 
 	/*
@@ -37,27 +50,54 @@ class genhtml
 			{
 			$name = "sahana_report";
 			}
-		$this->file_name = "/".$name.".html";
+		$this->file_name = $name;
 		}
 		
+	function setKeyword($keyword_in='')
+		{
+		$this->keyword = $keyword_in;
+		}
+
+	function setOwner($owner_in='')
+		{
+		$this->report_owner = $owner_in;
+		}
+
+	function setReportID($report_id_in = '')
+		{
+		$this->report_id = $report_id_in;
+		}
+
+
 	function addTitle($title_in='')
 		{
 		$this->output_code .= "<h3 align='".$this->title_pos."'>".$title_in."</h3>\n<br>\n";
+		$this->title_txt = $title_in;
 		}
 
 	function setTitlePos($title_pos_in='')
 		{
 		$this->title_pos = $title_pos_in;
 		}
-
-	function addSummary($summary_in='',$alignment='center')
+	
+	function addSummary($summary_in='')
 		{
-		$this->output_code .= "<p align=".$alignment.">".$summary_in."</p>\n<br>\n";
+		$this->output_code .= "<p align='".$this->summary_pos."'>".$summary_in."</p>\n<br>\n";
 		}
 
-	function setImage($img_path='')
+	function setSummaryPos($summary_pos_in="center")
 		{
-		$this->output_code .= "<img src='".$img_path."' align='left'/>\n<br>\n";
+		$this->summary_pos = $summary_pos_in;
+		} 
+
+	function addImage($img_path='')
+		{
+		$this->output_code .= "<img src='".$img_path."' align='".$this->image_pos."'/>\n<br>\n";
+		}
+
+	function setImagePos($img_pos_in='center')
+		{
+		$this->image_pos = $img_pos_in;
 		}
 
 	function lineBrake()
@@ -67,7 +107,7 @@ class genhtml
 	
 	function addTable($header_array='',$data_array='')
 		{
-		$this->output_code .= "<table border='1' align='center'>\n<tr>";
+		$this->output_code .= "<table border='1' align='".$this->table_pos."'>\n<tr>";
 		$header_keys = array_keys($header_array);
 		for($i=0;$i<count($header_array);$i++)
 			{
@@ -89,21 +129,20 @@ class genhtml
 			}
 			$this->output_code .= "</table>\n<br>\n";
 		}
+	
+	function setTablePos($table_pos_in='center')
+		{
+		$this->table_pos = $table_pos_in;	
+		}
 
 	function addLink($linkLocatoin='',$linkText='')
 		{
 		$this->output_code .= "<br><a href ='".$linkLocatoin."'>".$linkText."</a>";
 		}
 
-
 	function Output()
 		{
 		$this->output_code .= "</body>\n</html>\n";
-		//echo $this->output_code;
-
-
-		/**/
-
 		$_sd_path = str_replace('\\', '/', dirname(__FILE__));
 		$_sd_path = explode('/', dirname(__FILE__));
 		array_pop($_sd_path);
@@ -112,19 +151,16 @@ class genhtml
 		
 		$_sd_path = $_sd_path."/www/tmp/";
 		$data='';
-		//echo $_sd_path;
 
-			$f=fopen($_sd_path.$this->file_name,'wb');
+			$f=fopen($_sd_path.$this->file_name.$this->extention,'wb');
 			if(!$f)
 			$this->Error('Unable to create output file: '.$name);
 			fwrite($f,$this->output_code,strlen($this->output_code));
 			fclose($f);
-	
-		
-	
-		echo "Your chart/Graph has been created in ".$_sd_path;
-			/*
-		$fp = fopen($_sd_path.$this->output_file, "rb");
+
+		echo "Your XHTML report has been created in ".$_sd_path;
+			
+		$fp = fopen($_sd_path.$this->file_name.$this->extention, "rb");
 			while(!feof($fp)) 
 			{
 			$data .= fread($fp, 1024); 
@@ -132,27 +168,23 @@ class genhtml
 			fclose($fp);
 			$data = addslashes($data);
 			$data = addcslashes($data, "\0");
-
+		
 		$today = getdate();
 		$current_date = $today["year"]."-".$today["mon"]."-".$today["mday"];
 		$current_time = $today["hours"].":".$today["minutes"].":".$today["seconds"]; 
-		$file_size = filesize($_sd_path.$this->output_file)/1000; 
+		$file_size = filesize($_sd_path.$this->file_name.$this->extention)/1000; 
 		$file_type = $this->file_format; 
 		$title = $this->title_txt;
-		$file_name = $this->output_file;
+		$file_name = $this->file_name.$this->extention;
 		$the_keyword = $this->keyword;
-		$the_owner = $this->report_chart_owner;
-
+		$the_owner = $this->report_owner;
+		$the_rep_id = $this->report_id;
 
 		global $global;
     		$db=$global["db"];
-		$q="insert into report_files(file_name,file_data,date_of_created,time_of_created,report_chart_owner,file_type,file_size_kb,keyword,title) values ('$file_name','$data','$current_date','$current_time','$the_owner','$file_type','$file_size','$the_keyword','$title')";
+		$q="insert into report_files(rep_id,file_name,file_data,date_of_created,time_of_created,report_chart_owner,file_type,file_size_kb,keyword,title) values ('$the_rep_id','$file_name','$data','$current_date','$current_time','$the_owner','$file_type','$file_size','$the_keyword','$title')";
     		$res=$db->Execute($q);
 	
-
-	//-----------------------------------
-		*/
-
 		}
 
 
