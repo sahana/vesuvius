2fc526153e1afd8f74730720e773d164488f3acc
jayasinghe <>
2006-08-17 06:20:28 +0000

    sql script for creating reporting system tables added

---
+++ sahana-phase2/mod/rs/ins/dbcreate.sql
@@ -0,0 +1,14 @@
+use `sahana_main`;
+create table report_files
+	(rep_id varchar(100),
+	file_name varchar(100),
+	file_data longblob,
+	t_stamp timestamp default current_timestamp on update current_timestamp,
+	file_type varchar(10),
+	file_size_kb double,
+	title varchar(100));
+create table report_keywords
+	(rep_id varchar(100),
+	keyword_key varchar(100),
+	keyword varchar(100)	
+	);
