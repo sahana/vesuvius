f571bf04fa30eebb51e12b3a0abdf1f4723718d4
Joseph Priyanga Fonseka <hevayo@gmail.com>
2006-11-20 10:29:49 +0000

    date bug fix

--- sahana-phase2/www/res/js/scw.js
+++ sahana-phase2/www/res/js/scw.js
@@ -40,11 +40,11 @@
     // alternatively, hard code a date like this...
     //      var scwBaseYear = 1990;
 
-    var scwBaseYear        = scwDateNow.getFullYear()-10;
+    var scwBaseYear        = scwDateNow.getFullYear()-70;
 
     // How many years do want to be valid and to show in the drop-down list?
 
-    var scwDropDownYears   = 20;
+    var scwDropDownYears   = 80;
 
     // All language-dependent changes can be made here...
 
