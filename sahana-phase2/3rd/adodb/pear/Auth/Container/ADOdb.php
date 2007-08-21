7ca93a03151e36a356f892437fb370fc3473c603
mahesh <>
2007-08-21 11:21:31 +0000

    adodb files added

--- sahana-phase2/3rd/adodb/pear/Auth/Container/ADOdb.php
+++ sahana-phase2/3rd/adodb/pear/Auth/Container/ADOdb.php
@@ -17,7 +17,7 @@
 // |	Richard Tango-Lowy <richtl@arscognita.com>                                  |
 // +----------------------------------------------------------------------+
 //
-// $Id: ADOdb.php,v 1.1.1.1 2005-09-24 21:21:22 chamindra Exp $
+// $Id: ADOdb.php,v 1.2 2007-08-21 11:17:45 mahesh_kks Exp $
 //
 
 require_once 'Auth/Container.php';
@@ -35,7 +35,7 @@ require_once 'adodb-errorpear.inc.php';
  *
  * @author   Richard Tango-Lowy <richtl@arscognita.com>
  * @package  Auth
- * @version  $Revision: 1.1.1.1 $
+ * @version  $Revision: 1.2 $
  */
 class Auth_Container_ADOdb extends Auth_Container
 {
