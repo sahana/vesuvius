d0de5bc859ece39e91a542821f3a231cf64cc583
Fran Boon <flavour@partyvibe.com>
2008-04-27 20:09:48 +0000

    Wiki Markers' function changed to use FramedClouds & ability to open/close Popups multiple times :)

--- sahana-phase2/inc/lib_gis/plugins/openlayers/openlayers_fns.php
+++ sahana-phase2/inc/lib_gis/plugins/openlayers/openlayers_fns.php
@@ -8,7 +8,7 @@
 * @copyright    Lanka Software Foundation - http://www.opensource.lk
 * @package      Sahana - http://sahana.lk/
 * @library      GIS
-* @version      $Id: openlayers_fns.php,v 1.19 2008-04-27 17:01:53 franboon Exp $
+* @version      $Id: openlayers_fns.php,v 1.20 2008-04-27 20:09:48 franboon Exp $
 * @license      http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
 */
 
@@ -285,7 +285,7 @@
 		echo "}\n";
 		echo "if (popup == null) {\n";
 			echo "popup = feature$i.createPopup(true);\n";
-			echo "popup.setContentHTML(\"<b>$name</b><br /><a href='$url' target='_blank'>Link</a><br /></p>";
+			echo "popup.setContentHTML(\"<b>$name</b><br /><a href='$url'>Link</a><br /></p>";
 			echo "\");\n";
 			echo "popup.setBackgroundColor(\"white\");\n";
 			echo "popup.setOpacity(0.9);\n";
@@ -316,7 +316,10 @@
     var size = new OpenLayers.Size(20,34); // icon size
 	var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
 	var icon = new OpenLayers.Icon('<?=$folder?>marker.png',size,offset);
-	var popup;
+    var currentPopup;
+    AutoSizeFramedCloud = OpenLayers.Class(OpenLayers.Popup.FramedCloud, {
+            'autoSize': true
+        });
 <?php
     for($i=0;$i< sizeof($array);$i++){
         $lon=$array[$i]["lat"];
@@ -330,43 +333,37 @@
             $date="";
         }
         $author=($array[$i]["author"]!="")?$array[$i]["author"]:_("anonymous");
-        $edit=$array[$i]["edit"];
-        $id=$array[$i]["wiki_uuid"];
+        //ToDo: Implement Edit
+        //$edit=$array[$i]["edit"];
+        //$id=$array[$i]["wiki_uuid"];
+        echo "popupContentHTML = \"<b>$name</b><br>$desc<br><a href='$url' target='_blank'>Link</a><br>$date<br><b>Author</b>: $author</p>\"\n";
         echo "var lonlat = new OpenLayers.LonLat($lon,$lat);\n";
         echo "lonlat.transform(proj4326, proj900913);\n";
-        echo "var feature$i = new OpenLayers.Feature(markers, lonlat,{'icon': icon.clone()});\n";
-        echo "var marker$i = feature$i.createMarker();\n";
-        echo "markers.addMarker(marker$i);\n";
-        echo "marker$i.events.register(\"mousedown\", marker$i, mousedown$i);\n";
-        echo "function mousedown$i(evt) {\n";
-        // check to see if the popup was hidden by the close box
-        // if so, then destroy it before continuing
-        echo "if (popup != null) {\n";
-        echo "    if (!popup.visible()) {\n";
-        echo "        markers.map.removePopup(popup);\n";
-        echo "        popup.destroy();\n";
-        echo "        popup = null;\n";
-        echo "    }\n";
-        echo "}\n";
-        echo "if (popup == null) {\n";
-        echo "    popup = feature$i.createPopup(true);\n";
-        echo "    popup.setContentHTML(\"<b>$name</b><br>$desc<br><a href='$url' target='_blank'>Link</a><br>$date<br><b>Author</b>: $author</p>";
-        if (1 == $edit) {
-            echo "<br><a href='$url' target='_blank'>Edit</a>\");\n";
-        } else {
-            echo "\");\n";
-        }
-        echo "    popup.setBackgroundColor(\"white\");\n";
-        echo "    popup.setOpacity(0.9);\n";
-        echo "    markers.map.addPopup(popup);\n";
-        echo "} else {\n";
-        echo "    markers.map.removePopup(popup);\n";
-        echo "    popup.destroy();\n";
-        echo "    popup = null;\n";
-        echo "}\n";
-        echo "Event.stop(evt);\n";
-        echo "}\n";
+        echo "addMarker(lonlat,popupContentHTML);\n";
     }
+?>
+    function addMarker(lonlat, popupContentHTML) {
+        var feature = new OpenLayers.Feature(markers, lonlat,{'icon': icon.clone()}); 
+        feature.closeBox = 'true';
+        feature.popupClass = AutoSizeFramedCloud;
+        feature.data.popupContentHTML = popupContentHTML;
+        var marker = feature.createMarker();
+        var markerClick = function (evt) {
+            if (this.popup == null) {
+                this.popup = this.createPopup(this.closeBox);
+                this.popup.setOpacity(0.9);
+                map.addPopup(this.popup);
+                this.popup.show();
+            } else {
+                this.popup.toggle();
+            }
+            currentPopup = this.popup;
+            OpenLayers.Event.stop(evt);
+        };
+        marker.events.register("mousedown", feature, markerClick);
+        markers.addMarker(marker);
+    }
+<?php   
 }
 
 /**
@@ -415,7 +412,7 @@
 		echo "}\n";
 		echo "if (popup == null) {\n";
 			echo "popup = feature$i.createPopup(true);\n";
-			echo "popup.setContentHTML(\"<b>$name</b><br /><a href='$url' target='_blank'>Link</a><br /></p>";
+			echo "popup.setContentHTML(\"<b>$name</b><br /><a href='$url'>Link</a><br /></p>";
 			echo "\");\n";
 			echo "popup.setBackgroundColor(\"white\");\n";
 			echo "popup.setOpacity(0.9);\n";
