f0e82aacab79773880fe8c64d4a8ec03b0552569
Greg Miernicki <g@miernicki.com>
2009-08-06 22:34:22 +0000

    Added additional features to the module, namely... page visibility control, improved page interface in the admin section.

--- sahana-phase2/mod/rez/inst/dbcreate.sql
+++ sahana-phase2/mod/rez/inst/dbcreate.sql
@@ -1,6 +1,6 @@
 /**
 * modRez: The table structure to pages and their content for the resources tab.
-* Last changed: 20090420
+* Last changed: 20090806
 */
 
 DROP TABLE IF EXISTS `rez_pages`;
@@ -12,6 +12,7 @@ CREATE table rez_pages (
 	rez_menu_order 		int 				NOT NULL,
 	rez_content		mediumtext 			NOT NULL,
 	rez_description		varchar(128)			NOT NULL,
-	rez_timestamp		datetime			NOT NULL,
+	rez_timestamp		timestamp			NOT NULL,
+	rez_visibility		varchar(16)			NOT NULL,
 	PRIMARY KEY(rez_page_id)
 ) ENGINE=INNODB;
