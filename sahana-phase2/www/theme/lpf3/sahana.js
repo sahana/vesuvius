2d872268e76800cb2cf71c15b384de368edaeb9d
Greg Miernicki <g@miernicki.com>
2010-06-25 11:47:55 -0400

    Revisions to LPFv3 Theme Part 1

--- sahana-phase2/www/theme/lpf3/sahana.js
+++ sahana-phase2/www/theme/lpf3/sahana.js
@@ -1,8 +1,36 @@
 //function used to expand or collaps the mod_sub_menu.
 function switchLogin(from,to){
-	var fromBlock=document.getElementById(from).style;
-	var toBlock=document.getElementById(to).style;
-	fromBlock.display='none';
-	toBlock.display='block';
+	var fromBlock = document.getElementById(from).style;
+	var toBlock   = document.getElementById(to).style;
+	fromBlock.display = 'none';
+	toBlock.display   = 'block';
 }
 
+
+// function to redirect to a new page after a different event is selected
+function changeEvent(shortname) {
+	var i;
+	var t;
+	var found = false;
+	var pathArray = window.location.pathname.split('/');
+	var newPathname = '';
+	for (i=0; i < pathArray.length; i++) {
+		if (pathArray[i] == 'index.php') {
+			found = true;
+			break;
+		}
+	}
+	if (found) {
+		newPathname = pathArray[i];
+	} else {
+		newPathname = '';
+	}
+	t = window.location.protocol + '//' + window.location.host + '/' + shortname + '/' + newPathname + window.location.search;
+	window.location.href = t;
+}
+
+function checkEvent(shortname, defaultEvent) {
+	if (shortname == '') {
+		changeEvent(defaultEvent);
+	}
+}
