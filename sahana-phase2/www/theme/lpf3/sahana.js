1416c2559179d28bb4c15f1e5e23899fdd08b43f
Greg Miernicki <g@miernicki.com>
2010-07-12 20:15:09 -0400

    Correcting malformed url bug.

--- sahana-phase2/www/theme/lpf3/sahana.js
+++ sahana-phase2/www/theme/lpf3/sahana.js
@@ -9,23 +9,42 @@ function switchLogin(from,to){
 
 // function to redirect to a new page after a different event is selected
 function changeEvent(shortname) {
-	var i;
-	var t;
-	var found = false;
-	var pathArray = window.location.pathname.split('/');
-	var newPathname = '';
-	for (i=0; i < pathArray.length; i++) {
-		if (pathArray[i] == 'index.php') {
-			found = true;
-			break;
+	var i       = 0;
+	var j       = 0;
+	var path    = '';
+	var path2   = '';
+	var p       = window.location.pathname;
+	var pArray  = window.location.pathname.split('/');
+	var pLength = pArray.length;
+
+	// handle different url cases
+	// case 1>> path = '/'
+	// case 2>> path = '/index.php'
+	// case 3>> path = '/shortname'
+	// case 4>> path = '/shortname/'
+	// case 5>> path = '/shortname/index.php'
+	if(pLength<=3){
+		path='/';
+	}else{
+		// case 6>> path = '/~gmiernicki/sahanaDev/www/'
+		// case 7>> path = '/~gmiernicki/sahanaDev/www/index.php'
+		// case 8>> path = '/~gmiernicki/sahanaDev/www/shortname'
+		// case 9>> path = '/~gmiernicki/sahanaDev/www/shortname/'
+		// case 10>>path = '/~gmiernicki/sahanaDev/www/shortname/index.php'
+		for(i=0; i < pLength; i++) {
+			if(pArray[i] == 'www') {
+				path='/';
+				for(j=1; j<=i; j++) {
+					path = path+pArray[j]+'/';
+				}
+				break;
+			}
 		}
 	}
-	if (found) {
-		newPathname = pathArray[i];
-	} else {
-		newPathname = '';
+	if(pArray[pLength-1] == 'index.php'){
+		path2 = 'index.php';
 	}
-	t = window.location.protocol + '//' + window.location.host + '/' + shortname + '/' + newPathname + window.location.search;
+	var t = window.location.protocol+'//'+window.location.host+path+shortname+'/'+path2+window.location.search;
 	window.location.href = t;
 }
 
