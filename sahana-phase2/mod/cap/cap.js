860936d9782a520482729e5402cff9e940c3c52f
Greg Miernicki <g@miernicki.com>
2010-10-29 18:17:27 -0400

    moved mods to separate repo

--- sahana-phase2/mod/cap/cap.js
+++
@@ -1,5 +0,0 @@
-onload=update();
-function update(){
-setTimeout('update();',100000);
-xajax__shn_cap_update_alerts();
-}
--- sahana-phase2/mod/vm/templates/volunteer/please_login.tpl
+++
@@ -1,2 +0,0 @@
-<br />
-_("In order to begin, please log in using the Sahana account you just created.")
