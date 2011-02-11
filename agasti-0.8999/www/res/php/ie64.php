42b81a669a8fabad084f4550d534a91f521a13b0
Gregory Miernicki <gmiernicki@ceb-stage-lx.nlm.nih.gov>
2011-02-11 11:18:07 -0500

    0.9.0.5061 commit!

--- agasti-0.8999/www/res/php/ie64.php
+++
@@ -1,8 +0,0 @@
-<?php
-// from:http://goo.gl/Wo1uE
-// adds base64 decoding support to IE <= 7
-$data = split(";", $_SERVER["QUERY_STRING"]);
-$type = $data[0];
-$data = split(",", $data[1]);
-header("Content-type: ".$type);
-echo base64_decode($data[1]);
--- agasti-0.8999/www/theme/lpf3/info.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Theme Information File
-
-$shortname = "lpf3";
-$longname  = "Lost Person Finder v3";
-$siteTitle = "NLM People Locator";
-
