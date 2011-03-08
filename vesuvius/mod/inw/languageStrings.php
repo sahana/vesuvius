55dcc019e318cd8cbc06c91fe7c2c78a9687ac09
Greg Miernicki <g@miernicki.com>
2011-03-07 20:06:48 -0500

    header cleanup

--- vesuvius/mod/inw/languageStrings.php
+++ vesuvius/mod/inw/languageStrings.php
@@ -1,4 +1,17 @@
-<?
+<?php
+/**
+ * @name         Interactive Notification Wall (Search)
+ * @version      2.0
+ * @package      inw
+ * @author       Merwan Rodriguez <rodriguezmer@mail.nih.gov>
+ * @about        Developed in whole or part by the U.S. National Library of Medicine and the Sahana Foundation
+ * @link         https://pl.nlm.nih.gov/about
+ * @link         http://sahanafoundation.org
+ * @license	 http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
+ * @lastModified 2011.0307
+ */
+
+
 if (isset($_GET['l'])) {
     $l = $_GET['l'];
 } else {
