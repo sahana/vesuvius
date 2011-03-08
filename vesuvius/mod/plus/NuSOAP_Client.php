f44f8dd3cdae9dcd8c6bbc6333f3106caf2cd0e4
Greg Miernicki <g@miernicki.com>
2011-03-08 14:30:33 -0500

    plus header / structure update

--- vesuvius/mod/plus/NuSOAP_Client.php
+++ vesuvius/mod/plus/NuSOAP_Client.php
@@ -1,4 +1,15 @@
 <?
+/**
+ * @name         PL User Services
+ * @version      2.0.0
+ * @package      plus
+ * @author       Greg Miernicki <g@miernicki.com> <gregory.miernicki@nih.gov>
+ * @about        Developed in whole or part by the U.S. National Library of Medicine
+ * @link         https://pl.nlm.nih.gov/about
+ * @license	 http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
+ * @lastModified 2011.0308
+ */
+
 // nusoap client test
 
 // load nusoap client library
