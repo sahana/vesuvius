1e305261dd34da6c98c2c8597648d3946264d716
Greg Miernicki <g@miernicki.com>
2011-03-07 20:24:23 -0500

    header update

--- vesuvius/mod/em/em.js
+++ vesuvius/mod/em/em.js
@@ -3,11 +3,13 @@
  * @version      1.1
  * @package      em
  * @author       Greg Miernicki <g@miernicki.com> <gregory.miernicki@nih.gov>
- * @about        Developed in whole or part by the U.S. National Library of Medicine
+ * @about        Developed in whole or part by the U.S. National Library of Medicine and the Sahana Foundation
  * @link         https://pl.nlm.nih.gov/about
+ * @link         http://sahanafoundation.org
  * @license	 http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
- * @lastModified 2011.0302
+ * @lastModified 2011.0307
  */
+
 
 em_show_message('Loading Events...');
 setTimeout('em_show_events();', 1500);
