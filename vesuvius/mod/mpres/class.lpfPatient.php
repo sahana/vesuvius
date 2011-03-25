b19b1b2d8becc81ad16a59ef0b26de754cca5874
Greg Miernicki <g@miernicki.com>
2011-03-24 20:47:13 -0400

    updated mpres for new db schema v1.7

--- vesuvius/mod/mpres/class.lpfPatient.php
+++ vesuvius/mod/mpres/class.lpfPatient.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @name         MPR Email Service
- * @version      1.6
+ * @version      1.7
  * @package      mpres
  * @author       Greg Miernicki <g@miernicki.com> <gregory.miernicki@nih.gov>
  * @about        Developed in whole or part by the U.S. National Library of Medicine and the Sahana Foundation
  * @link         https://pl.nlm.nih.gov/about
  * @link         http://sahanafoundation.org
  * @license	 http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
- * @lastModified 2011.0308
+ * @lastModified 2011.0324
  */
 
 
@@ -76,54 +76,42 @@ class lpfPatient {
 	}
 
 
+
 	public function insertPersonXMLv12() {
 		global $global;
 		$this->figureOutIncidentIdv12();
 		$this->insertImages();
 
 		// insert person
-		$q1 = " INSERT INTO person_uuid (p_uuid, full_name, family_name, l10n_name, given_name)
-			VALUES ('".$this->uuid."', '".$this->firstName." ".$this->familyName."', '".$this->familyName."', NULL, '".$this->firstName."');";
+		$q1 = "
+			INSERT INTO person_uuid (p_uuid, full_name, family_name, l10n_name, given_name, incident_id, hospital_uuid)
+			VALUES ('".$this->uuid."', '".$this->firstName." ".$this->familyName."', '".$this->familyName."', NULL, '".$this->firstName."', '".$this->incident_id."', '".$this->hospitalId."');
+		";
 		$res = $global['db']->Execute($q1);
-/*
-		// insert person missing info if missing
-		if($this->sahanaStatus == "mis") {
-			$q2 = " INSERT INTO person_missing (p_uuid, last_seen, last_clothing, comments)
-				VALUES ('".$this->uuid."', NULL, NULL, '".$this->comments."');";
-			$res = $global['db']->Execute($q2);
-		}
-*/
-		// insert an assignment of this person to the correct incident
-		$q3 = " INSERT INTO resource_to_incident (x_uuid, incident_id)
-			VALUES ('".$this->uuid."', '".$this->incident_id."');";
-		$res = $global['db']->Execute($q3);
+
 
 		// insert person's status
-		$q4 = " INSERT INTO person_status (p_uuid, opt_status, updated, isvictim, updated_server)
-			VALUES ('".$this->uuid."', '".$this->sahanaStatus."', '".$this->clientDate."', TRUE, CURRENT_TIMESTAMP);";
+		$q4 = "
+			INSERT INTO person_status (p_uuid, opt_status, updated, isvictim, updated_server)
+			VALUES ('".$this->uuid."', '".$this->sahanaStatus."', '".$this->clientDate."', TRUE, CURRENT_TIMESTAMP);
+		";
 		$res = $global['db']->Execute($q4);
 
 		// insert person's details
-		$q5 = " INSERT INTO person_details (p_uuid, opt_age_group, opt_gender, years_old, minAge, maxAge)
-			VALUES ('".$this->uuid."', NULL, '".$this->gender."', ".$this->age.", ".$this->minAge.", ".$this->maxAge.");";
+		$q5 = "
+			INSERT INTO person_details (p_uuid, opt_age_group, opt_gender, years_old, minAge, maxAge)
+			VALUES ('".$this->uuid."', NULL, '".$this->gender."', ".$this->age.", ".$this->minAge.", ".$this->maxAge.");
+		";
 		$res = $global['db']->Execute($q5);
 
 		// insert into mpres_log
-		$q7 = " INSERT INTO mpres_log (p_uuid, email_subject, email_from, email_date, update_time)
-			VALUES ('".$this->uuid."','".$this->emailSubject."','".$this->emailFrom."','".$this->emailDate."',NOW());";
+		$q7 = "
+			INSERT INTO mpres_log (p_uuid, email_subject, email_from, email_date, update_time)
+			VALUES ('".$this->uuid."','".$this->emailSubject."','".$this->emailFrom."','".$this->emailDate."',NOW());
+		";
 		$res = $global['db']->Execute($q7);
-
-		// insert into mpres_log
-		$q8 = " INSERT INTO person_to_hospital (hospital_uuid, p_uuid)
-			VALUES ('".$this->hospitalId."', '".$this->uuid."');";
-		$res = $global['db']->Execute($q8);
-
-/*
-		echo "\n---------------------------------------------------------------------\n";
-		echo $q1."\n\n".$q3."\n\n".$q4."\n\n".$q5."\n\n".$q7;
-		echo "\n---------------------------------------------------------------------\n";
-*/
 	}
+
 
 
 	public function insertPersonXMLv16() {
@@ -132,35 +120,41 @@ class lpfPatient {
 		$this->insertImages();
 
 		// insert person
-		$q1 = " INSERT INTO person_uuid (p_uuid, full_name, family_name, l10n_name)
-			VALUES ('".$this->uuid."', '".$this->firstName." ".$this->familyName."', '".$this->familyName."', NULL);";
+		$q1 = "
+			INSERT INTO person_uuid (p_uuid, full_name, family_name, l10n_name, incident_id)
+			VALUES ('".$this->uuid."', '".$this->firstName." ".$this->familyName."', '".$this->familyName."', NULL, '".$this->incident_id."');
+		";
 		$res = $global['db']->Execute($q1);
 
 		// insert person missing info
-		$q2 = " INSERT INTO person_missing (p_uuid, last_seen, last_clothing, comments)
-			VALUES ('".$this->uuid."', NULL, NULL, '".$this->comments."');";
+		$q2 = "
+			INSERT INTO person_missing (p_uuid, last_seen, last_clothing, comments)
+			VALUES ('".$this->uuid."', NULL, NULL, '".$this->comments."');
+		";
 		$res = $global['db']->Execute($q2);
 
-		// insert an assignment of this person to the correct incident
-		$q3 = " INSERT INTO resource_to_incident (x_uuid, incident_id)
-			VALUES ('".$this->uuid."', '".$this->incident_id."');";
-		$res = $global['db']->Execute($q3);
-
 		// insert person's status
-		$q4 = " INSERT INTO person_status (p_uuid, opt_status, updated, isvictim)
-			VALUES ('".$this->uuid."', '".$this->sahanaStatus."', CURRENT_TIMESTAMP, TRUE);";
+		$q4 = "
+			INSERT INTO person_status (p_uuid, opt_status, updated, isvictim)
+			VALUES ('".$this->uuid."', '".$this->sahanaStatus."', CURRENT_TIMESTAMP, TRUE);
+		";
 		$res = $global['db']->Execute($q4);
 
 		// insert person's details
-		$q5 = " INSERT INTO person_details (p_uuid, opt_age_group, opt_gender, years_old)
-			VALUES ('".$this->uuid."', NULL, '".$this->gender."', '".$this->age."');";
+		$q5 = "
+			INSERT INTO person_details (p_uuid, opt_age_group, opt_gender, years_old)
+			VALUES ('".$this->uuid."', NULL, '".$this->gender."', '".$this->age."');
+		";
 		$res = $global['db']->Execute($q5);
 
 		// insert into mpres_log
-		$q7 = " INSERT INTO mpres_log (p_uuid, email_subject, email_from, email_date, update_time)
-			VALUES ('".$this->uuid."','".$this->emailSubject."','".$this->emailFrom."','".$this->emailDate."',NOW());";
+		$q7 = "
+			INSERT INTO mpres_log (p_uuid, email_subject, email_from, email_date, update_time)
+			VALUES ('".$this->uuid."','".$this->emailSubject."','".$this->emailFrom."','".$this->emailDate."',NOW());
+		";
 		$res = $global['db']->Execute($q7);
 	}
+
 
 
 	public function insertPerson() {
@@ -169,43 +163,46 @@ class lpfPatient {
 		$this->insertImages();
 
 		// insert person
-		$q1 = " INSERT INTO person_uuid (p_uuid, full_name, family_name, l10n_name)
-			VALUES ('".$this->uuid."','".$this->emailSubject."',NULL,NULL);";
+		$q1 = "
+			INSERT INTO person_uuid (p_uuid, full_name, family_name, l10n_name, incident_id)
+			VALUES ('".$this->uuid."','".$this->emailSubject."', NULL, NULL, ,'".$this->incident_id."');
+		";
 		$res = $global['db']->Execute($q1);
 
 		// insert person's missing status
-		$q2 = " INSERT INTO person_missing (p_uuid, last_seen, last_clothing, comments)
-			VALUES ('".$this->uuid."', NULL, NULL, NULL);";
+		$q2 = "
+			INSERT INTO person_missing (p_uuid, last_seen, last_clothing, comments)
+			VALUES ('".$this->uuid."', NULL, NULL, NULL);
+		";
 		$res = $global['db']->Execute($q2);
 
-		// insert an assignment of this person to the correct incident
-		$q3 = " INSERT INTO resource_to_incident (x_uuid, incident_id)
-			VALUES ('".$this->uuid."','".$this->incident_id."');";
-		$res = $global['db']->Execute($q3);
-
 		// insert person's status
-		$q4 = " INSERT INTO person_status (p_uuid, opt_status, updated, isvictim)
-			VALUES ( '".$this->uuid."', '".$this->sahanaStatus."', CURRENT_TIMESTAMP, TRUE);";
+		$q4 = "
+			INSERT INTO person_status (p_uuid, opt_status, updated, isvictim)
+			VALUES ( '".$this->uuid."', '".$this->sahanaStatus."', CURRENT_TIMESTAMP, TRUE);
+		";
 		$res = $global['db']->Execute($q4);
 
 		// insert into mpres_log
-		$q5 = " INSERT INTO mpres_log (p_uuid, email_subject, email_from, email_date, update_time)
-			VALUES ('".$this->uuid."','".$this->emailSubject."','".$this->emailFrom."','".$this->emailDate."',NOW());";
+		$q5 = "
+			INSERT INTO mpres_log (p_uuid, email_subject, email_from, email_date, update_time)
+			VALUES ('".$this->uuid."','".$this->emailSubject."','".$this->emailFrom."','".$this->emailDate."',NOW());
+		";
 		$res = $global['db']->Execute($q5);
 
 		// insert person's details
-		$q6 = " INSERT INTO person_details (p_uuid, opt_age_group, opt_gender, years_old)
-			VALUES ('".$this->uuid."', NULL, NULL, NULL);";
+		$q6 = "
+			INSERT INTO person_details (p_uuid, opt_age_group, opt_gender, years_old)
+			VALUES ('".$this->uuid."', NULL, NULL, NULL);
+		";
 		$res = $global['db']->Execute($q6);
 
 		// insert person who reported link to root cuz we dont really know who reported them
-		$q7 = " INSERT INTO person_to_report (p_uuid, rep_uuid)
-			VALUES ('".$this->uuid."', '1');";
+		$q7 = "
+			INSERT INTO person_to_report (p_uuid, rep_uuid)
+			VALUES ('".$this->uuid."', '1');
+		";
 		$res = $global['db']->Execute($q7);
-
-		//echo "\n---------------------------------------------------------------------\n";
-		//echo $q1."\n\n".$q2."\n\n".$q3."\n\n".$q4."\n\n".$q5."\n\n".$q6."\n\n".$q7;
-		//echo "\n---------------------------------------------------------------------\n";
 	}
 
 
@@ -225,11 +222,16 @@ class lpfPatient {
 
 	public function figureOutIncidentId() {
 		global $global;
-		$query  = "SELECT * FROM incident WHERE shortname = '".$this->shortName."';";
+		$query  = "
+			SELECT *
+			FROM incident WHERE shortname = '".$this->shortName."';
+		";
 		$result = $global['db']->Execute($query);
 		$row    = $result->FetchRow();
 		$this->incident_id = $row['incident_id'];
 	}
+
+
 
 	public function figureOutIncidentIdv12() {
 		global $global;
