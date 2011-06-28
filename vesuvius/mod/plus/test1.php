7ea049dbd5e3302735770cf3de032eb94b8bb94d
Greg Miernicki <g@miernicki.com>
2011-06-28 17:05:53 -0400

    search work...

--- vesuvius/mod/plus/test1.php
+++ vesuvius/mod/plus/test1.php
@@ -14,7 +14,8 @@ $user = "testDontDelete";
 $pass = "dontDelete99";
 
 require_once("../../3rd/nusoap/lib/nusoap.php");
-$wsdl = "https://plstage.nlm.nih.gov/~miernickig/sahanaDev/www/index.php?wsdl&api=1.9.3";
+//$wsdl = "https://plstage.nlm.nih.gov/~miernickig/sahanaDev/www/index.php?wsdl&api=1.9.3";
+$wsdl = "http://localhost/vesuvius-trunk/vesuvius/www/index.php?wsdl&api=1.9.4";
 //$wsdl = "https://pl.nlm.nih.gov/?wsdl";
 $client = new nusoap_client($wsdl);
 
@@ -23,6 +24,6 @@ $client = new nusoap_client($wsdl);
 //$result = $client->call('changeUserPassword', array('username'=>$user, 'oldPassword'=>$pass, 'newPassword'=>$pass));
 //$result = $client->call('resetUserPassword', array('username'=>"testCaseUser9"));
 //$result = $client->call('forgotUsername', array('email'=>"testCase@email.com"));
-$result = $client->call('getUserGroup', array('username'=>$user));
+$result = $client->call('getHospitalList', array(null));
 
 echo "<pre>".print_r($result, true)."</pre>";
