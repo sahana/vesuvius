38c88c9dcbab146a6ac035cb7047bd8432973545
Greg Miernicki <g@miernicki.com>
2011-07-11 19:12:11 -0400

    re-added stat module after cleanup + pfif changes

---
+++ vesuvius/mod/stat/sql/install.sql
@@ -0,0 +1,14 @@
+/**
+ * @name         Statistics
+ * @version      0.1
+ * @package      stat
+ * @author       Lan Le <lale@mail.nih.gov>
+ * @author       Greg Miernicki <g@miernicki.com> <gregory.miernicki@nih.gov>
+ * @about        Developed in whole or part by the U.S. National Library of Medicine
+ * @link         https://pl.nlm.nih.gov/about
+ * @license	 http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
+ * @lastModified 2011.0711
+ */
+
+
+/* if the module is installed on a clean system... this is all the sql that should be executed to create the necessary database structures */
--- vesuvius/mod/stat/stat.js
+++ vesuvius/mod/stat/stat.js
@@ -1,18 +1,16 @@
-<?php
 /**
- * @name         Person Finder Interchange Format
+ * @name         Statistics
  * @version      0.1
- * @package      pfif
- * @author       Leif Neve <lneve@mail.nih.gov>
+ * @package      stat
+ * @author       Lan Le <lale@mail.nih.gov>
  * @author       Greg Miernicki <g@miernicki.com> <gregory.miernicki@nih.gov>
  * @about        Developed in whole or part by the U.S. National Library of Medicine
  * @link         https://pl.nlm.nih.gov/about
  * @license	 http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
- * @lastModified 2011.0622
+ * @lastModified 2011.0711
  */
 
 
-$conf['mod_pfif_name']         = _t("PFIF Admin");
-$conf['mod_pfif_menuorder']    = 70;
-$conf['mod_pfif_version']      = 0.1;
-$conf['mod_pfif_dependencies'] = "";
+function call_hello() {
+	alert('hello javascript galaxy');
+}
