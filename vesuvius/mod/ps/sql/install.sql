dc47b4f1b9b3c9b5fbc058fe9e3427d9de816319
Greg Miernicki <g@miernicki.com>
2011-07-11 18:24:42 -0400

    added ps skeleton module

---
+++ vesuvius/mod/ps/sql/install.sql
@@ -0,0 +1,15 @@
+/**
+ * @name         Person Finder Interchange Format
+ * @version      0.1
+ * @package      pfif
+ * @author       Leif Neve <lneve@mail.nih.gov>
+ * @author       Greg Miernicki <g@miernicki.com> <gregory.miernicki@nih.gov>
+ * @about        Developed in whole or part by the U.S. National Library of Medicine
+ * @link         https://pl.nlm.nih.gov/about
+ * @license	 http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
+ * @lastModified 2011.0622
+ */
+
+
+
+/* if the module is installed on a clean system... this is all the sql that should be executed */
