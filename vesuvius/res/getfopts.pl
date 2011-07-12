96b1f9b7c47604fdb22e6e5b240abc6322032ba6
Greg Miernicki <g@miernicki.com>
2011-07-11 20:22:54 -0400

    added some gsoc revisions per locale stuff

--- vesuvius/res/getfopts.pl
+++ vesuvius/res/getfopts.pl
@@ -3,7 +3,7 @@
 # Perl Helper Script to extract field_options descriptions
 # from Sahana SQL files, useful to create the PO template
 #
-# This creates PHP code using _("") as gettext-Alias for each
+# This creates PHP code using _t("") as gettext-Alias for each
 # field_options description, and writes it to stdout
 #
 # author: dominic
@@ -12,7 +12,7 @@ print "<?php\n";
 
 while( <STDIN> ) {
   if ( /.*INSERT\s*INTO\s*field_options.*VALUES\s*\(.*,\s*\'([^']*)\'\s*\)\s*;/ ) {
-    print "print(_(\"$1\"));\n";
+    print "print(_t(\"$1\"));\n";
   }
 }
 
