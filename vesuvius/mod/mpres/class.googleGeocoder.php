bc086cb8027e02811851ad09acde4fb6c329563e
Greg Miernicki <g@miernicki.com>
2011-07-13 18:11:53 -0400

    cleanup

--- vesuvius/mod/mpres/class.googleGeocoder.php
+++ vesuvius/mod/mpres/class.googleGeocoder.php
@@ -176,3 +176,28 @@ class googlePoint {
 		return new self($latitude, $longitude);
 	}
 }
+
+
+
+/* TEST CASE
+
+$address = "301 Bayside Road, St. Leonard, MD";
+
+require_once('class.googleGeocoder.php');
+$geocoder = new googleGeocoder();
+try {
+	$placemarks = $geocoder->lookup($address);
+} catch(Exception $ex) {
+	echo $ex->getMessage();
+	exit;
+}
+if (count($placemarks) > 0) {
+	foreach ($placemarks as $placemark) {
+		echo htmlSpecialChars($placemark)."(".$placemark->getPoint()->getLatitude().",".$placemark->getPoint()->getLongitude().")\n";
+	}
+} else {
+	echo "no matches";
+}
+
+*/
+
