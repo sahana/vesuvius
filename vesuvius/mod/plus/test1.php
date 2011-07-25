6819f8aaf301a9d31d006cc1851edc3edcccc435
Greg Miernicki <g@miernicki.com>
2011-07-25 19:01:57 -0400

    added reference xmls

--- vesuvius/mod/plus/test1.php
+++ vesuvius/mod/plus/test1.php
@@ -14,7 +14,7 @@ $user = "testDontDelete";
 $pass = "dontDelete99";
 
 require_once("../../3rd/nusoap/lib/nusoap.php");
-$wsdl = "https://plstage.nlm.nih.gov/~miernickig/sahanaDev/www/index.php?wsdl&api=1.9.5";
+$wsdl = "https://plstage.nlm.nih.gov/~miernickig/vesuvius/vesuvius/www/index.php?wsdl&api=1.9.5";
 //$wsdl = "http://localhost/vesuvius-trunk/vesuvius/www/index.php?wsdl&api=1.9.4";
 //$wsdl = "https://pl.nlm.nih.gov/?wsdl";
 $client = new nusoap_client($wsdl);
