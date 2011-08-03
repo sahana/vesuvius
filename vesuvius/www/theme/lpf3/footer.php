d9a6f66efc3fb69f01180af3cd3cdf34b59edb46
Greg Miernicki <g@miernicki.com>
2011-08-03 16:38:08 -0400

    footer, mpres design fixes

--- vesuvius/www/theme/lpf3/footer.php
+++ vesuvius/www/theme/lpf3/footer.php
@@ -8,13 +8,11 @@
  * @link         https://pl.nlm.nih.gov/about
  * @link         http://sahanafoundation.org
  * @license	 http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
- * @lastModified 2011.0308
+ * @lastModified 2011.0803
  */
 
-/*
-// array of events
-$events = array();
-*/
+
+$legalese = "Notice: Submission of information is voluntary. All submitted information will be made publicly available. OMB NO: 0925-0612 EXPIRATION DATE: 6/30/2013 Public reporting burden for this collection of information is estimated to average 0.08 hours per response. This estimate includes the time for reviewing instructions, gathering, and entering data. An agency may not conduct or sponsor, and a person is not required to respond to, a collection of information unless it displays a currently valid OMB control number. Send comments regarding this burden estimate or any other aspect of this collection of information, including suggestions for reducing this burden, to: NIH, Project Clearance Branch, 6705 Rockledge Drive, MSC 7974, Bethesda, MD 20892-7974, ATTN: PRA (0925-0612). Do not return the completed form to this address.";
 
 ?>
 <div id="blueBack">&nbsp;</div>
@@ -42,7 +40,7 @@ $events = array();
 		</td>
 		</tr>
 	</table>
-	<div id="notice">Notice: Submission of information is voluntary. All submitted information will be made publicly available. OMB NO: 0925-0612 EXPIRATION DATE: 6/30/2013 Public reporting burden for this collection of information is estimated to average 0.08 hours per response. This estimate includes the time for reviewing instructions, gathering, and entering data. An agency may not conduct or sponsor, and a person is not required to respond to, a collection of information unless it displays a currently valid OMB control number. Send comments regarding this burden estimate or any other aspect of this collection of information, including suggestions for reducing this burden, to: NIH, Project Clearance Branch, 6705 Rockledge Drive, MSC 7974, Bethesda, MD 20892-7974, ATTN: PRA (0925-0612). Do not return the completed form to this address.
+	<div id="notice" onclick="alert('<?php echo $legalese; ?>');"><?php echo $legalese; ?>
 	<?php
 		// show build & version
 
@@ -80,85 +78,4 @@ $events = array();
 </div>
 <?php
 
-/*
-function addChildren($id, $spacer) {
-	global $events;
-	global $global;
-	$q = "
-		SELECT *
-		FROM incident
-		WHERE parent_id = '".$id."'
-		ORDER BY date DESC;
-	";
-	$res = $global['db']->Execute($q);
 
-	// add child to the array and then find its children recursively
-	while($row = $res->FetchRow() ){
-		$row['name'] = $spacer.$row['name'];
-		$events[] = $row;
-		addChildren($row['incident_id'], $spacer."&nbsp;&nbsp;&nbsp;&nbsp;");
-	}
-}
-
-
-
-// Displays the Event/Disaster Dropdown menu and allows redirection via the js code
-function selekta() {
-	global $global;
-	global $conf;
-	global $events;
-
-	echo "<select id=\"disasterList\" onchange=\"changeEvent(this.value);\">";
-
-	if(isset($_SESSION['group_id'])) {
-		$groupSql = "(private_group IS NULL OR private_group = '".$_SESSION['group_id']."')";
-	} else {
-		$groupSql = "private_group IS NULL";
-	}
-
-	$q = "
-		SELECT *
-		FROM incident
-		WHERE parent_id is NULL
-		AND ".$groupSql."
-		ORDER BY date DESC;
-	";
-	$res = $global['db']->Execute($q);
-
-	// add disasters to the array
-	while($row = $res->FetchRow() ){
-		$events[] = $row;
-		addChildren($row['incident_id'], "&nbsp;&nbsp;&nbsp;&nbsp;");
-	}
-
-	foreach($events as $row) {
-		if($_GET['shortname'] == $row['shortname']) {
-			$selected = "selected";
-		} else {
-			$selected = "";
-		}
-		if($row['closed'] == "1") {
-			echo "<option ".$selected." value=\"".$row['shortname']."\" class=\"110 grey\">".$row['name']."</option>";
-		} else {
-			echo "<option ".$selected." value=\"".$row['shortname']."\" class=\"110\">".$row['name']."</option>";
-		}
-	}
-	echo "</select>";
-}
-
-
-
-// Check whether we are on the default event when we enter the site
-function getDefaultEvent() {
-	global $global;
-	global $conf;
-
-	$sql = "SELECT shortname FROM incident WHERE `default` = 1 LIMIT 1;";
-	$arr = $global['db']->GetAll($sql);
-	if (!empty($arr)) {
-		foreach($arr as $row) {
-			echo $row['shortname'];
-		}
-	}
-}
-*/
