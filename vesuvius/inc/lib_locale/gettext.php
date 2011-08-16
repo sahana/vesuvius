6c1c11e994e070190fb7ccea668d27b0cdb4b4c4
Greg Miernicki <g@miernicki.com>
2011-08-16 12:41:41 -0400

    patched gettext (test)

--- vesuvius/inc/lib_locale/gettext.php
+++ vesuvius/inc/lib_locale/gettext.php
@@ -111,10 +111,8 @@ class gettext_reader {
     // Caching can be turned off
     $this->enable_cache = $enable_cache;
 
-    // $MAGIC1 = (int)0x950412de; //bug in PHP 5
-    $MAGIC1 = (int) - 1794895138;
-    // $MAGIC2 = (int)0xde120495; //bug
-    $MAGIC2 = (int) - 569244523;
+     $MAGIC1 = (int)0x950412de;
+     $MAGIC2 = (int)0xde120495;
 
     $this->STREAM = $Reader;
     $magic = $this->readint();
