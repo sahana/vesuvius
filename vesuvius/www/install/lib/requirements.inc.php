0f7665c0d7c25fab955ef30897aa2a222d03d6f7
chinthakarukshan <chinthakarukshan@gmail.com>
2011-08-20 00:55:29 +0530

    replace the func.inc.php file and requirements.inc.php file with the edited appropriate files.

--- vesuvius/www/install/lib/requirements.inc.php
+++ vesuvius/www/install/lib/requirements.inc.php
@@ -18,6 +18,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
+define("SPACE", " ");
 function check_php_version()
 {
   $required = '5.2.12';
