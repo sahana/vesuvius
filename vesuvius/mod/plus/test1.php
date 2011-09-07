5bb4534749c56012f3b31a46e45274449130a870
Greg Miernicki <g@miernicki.com>
2011-09-07 12:47:56 -0400

    fixes improper gender mapping

--- vesuvius/mod/plus/test1.php
+++ vesuvius/mod/plus/test1.php
@@ -14,8 +14,8 @@ $user = "testDontDelete";
 $pass = "dontDelete99";
 
 require_once("../../3rd/nusoap/lib/nusoap.php");
-//$wsdl = "https://plstage.nlm.nih.gov/?wsdl&api=1.9.5";
-$wsdl = "http://plstage.nlm.nih.gov/~miernickig/vesuvius/vesuvius/www/index.php?wsdl&api=1.9.5";
+//$wsdl = "https://plstage.nlm.nih.gov/?wsdl&api=1.9.6";
+$wsdl = "http://plstage.nlm.nih.gov/~miernickig/vesuvius/vesuvius/www/index.php?wsdl&api=1.9.6";
 //$wsdl = "https://pl.nlm.nih.gov/?wsdl";
 $client = new nusoap_client($wsdl);
 
