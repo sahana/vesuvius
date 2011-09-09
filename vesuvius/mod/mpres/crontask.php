cc23a918f589db2bc96e24a7916d1446cea85373
Greg Miernicki <g@miernicki.com>
2011-09-08 22:10:03 -0400

    mpres 2.0

--- vesuvius/mod/mpres/crontask.php
+++ vesuvius/mod/mpres/crontask.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @name         MPR Email Service
- * @version      1.9
+ * @version      2.0
  * @package      mpres
  * @author       Greg Miernicki <g@miernicki.com> <gregory.miernicki@nih.gov>
  * @about        Developed in whole or part by the U.S. National Library of Medicine and the Sahana Foundation
  * @link         https://pl.nlm.nih.gov/about
  * @link         http://sahanafoundation.org
  * @license	 http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
- * @lastModified 2011.0728
+ * @lastModified 2011.0908
  */
 
 // cron job for mpres
@@ -17,14 +17,13 @@
 $global['approot'] = getcwd()."/../../";
 
 // include required libraries
-require_once("class.mpres.php");
-require_once("class.lpfPatient.php");
 require_once("../../conf/sahana.conf");
 require_once("../../3rd/adodb/adodb.inc.php");
 require_once("../../inc/handler_db.inc");
 require_once("../../inc/lib_uuid.inc");
 require_once("../../inc/lib_image.inc");
 require_once("../../mod/lpf/lib_lpf.inc");
+require_once("class.mpres.php");
 
 $m = new mpres();
 $m->loopInbox();
