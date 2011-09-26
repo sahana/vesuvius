93795a39e4470d8cf8af366a2249e1b50dad2097
Greg Miernicki <g@miernicki.com>
2011-09-26 12:18:06 -0400

    HA v21 adds timestamps to the legalese texts denoting when they were last updated which is provided in PLUS v198

--- vesuvius/mod/plus/test1.php
+++ vesuvius/mod/plus/test1.php
@@ -20,7 +20,7 @@ $wsdl = "http://plstage.nlm.nih.gov/~miernickig/vesuvius/vesuvius/www/index.php?
 
 $client = new nusoap_client($wsdl);
 
-//$result = $client->call('getHospitalPolicy', array('hospital_uuid'=>1));
+$result = $client->call('getHospitalLegaleseTimestamps', array('hospital_uuid'=>131));
 //$result = $client->call('registerUser', array('username'=>'testCaseUser', 'emailAddress'=>'testCase@email.com', 'password'=>'testPassword99', 'givenName'=>'testCaseGiven', 'familyName'=>'testCaseFamily'));
 //$result = $client->call('changeUserPassword', array('username'=>$user, 'oldPassword'=>$pass, 'newPassword'=>$pass));
 //$result = $client->call('resetUserPassword', array('username'=>"testCaseUser9"));
@@ -65,7 +65,7 @@ $result = $client->call('searchWithAuth', array(
 
 //$x = file_get_contents("TP.xml");
 $x = file_get_contents("reference_TRIAGEPIC1.xml");
-$result = $client->call('reportPerson', array('personXML'=>$x, 'eventShortName'=>'test', 'xmlFormat'=>'TRIAGEPIC1', 'username'=>$user, 'password'=>$pass));
+//$result = $client->call('reportPerson', array('personXML'=>$x, 'eventShortName'=>'test', 'xmlFormat'=>'TRIAGEPIC1', 'username'=>$user, 'password'=>$pass));
 
 echo "<pre>wsdl >> ".$wsdl."\n\n".var_export($result, true)."</pre>";
 
