c0ebf6dfe340cb90cb54a2555913a01d719166cc
Greg Miernicki <g@miernicki.com>
2011-10-27 18:00:34 -0400

    v03 of stat module, numerous bug fixes

--- vesuvius/mod/stat/test.php
+++ vesuvius/mod/stat/test.php
@@ -1,26 +1,42 @@
 <?php
+/**
+ * @name         Statistics
+ * @version      0.3
+ * @package      stat
+ * @author       Lan Ngoc Le <lale@mail.nih.gov>
+ * @author       Greg Miernicki <g@miernicki.com> <gregory.miernicki@nih.gov>
+ * @about        Developed in whole or part by the U.S. National Library of Medicine
+ * @link         https://pl.nlm.nih.gov/about
+ * @license	 http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
+ * @lastModified 2011.1027
+ */
+
+
+// testing script....
+
+
 	$q="SELECT DATE( expiry_date ) AS expiry_date, count( p_uuid ) AS count_puuid
 	FROM `person_uuid` a
 	JOIN incident b ON a.incident_id = b.incident_id
 	WHERE (
 	b.shortname LIKE '".$shortname."'
-	AND a.expiry_date IS NOT NULL 
+	AND a.expiry_date IS NOT NULL
 	AND a.expiry_date > '2011-10-31'
 	AND a.expiry_date < '2011-11-31')
-	GROUP BY DATE( expiry_date ) 
+	GROUP BY DATE( expiry_date )
 	ORDER BY expiry_date";
-	//$res=mysql_query($q);	
+	//$res=mysql_query($q);
 	$res = $global['db']->Execute($q);
 	//$row = mysql_fetch_array($res);
 	$row = $res->FetchRow();
 	/*check result */
 	if($row === FALSE) {
-    	die(mysql_error()); 
+    	die(mysql_error());
 	}
 	$json_expiry_date = array();
 	$json_count_puuid = array();
 	do{
-		$expiry_date[] = (strtotime($row['expiry_date'])*1000);		
+		$expiry_date[] = (strtotime($row['expiry_date'])*1000);
 		//date_default_timezone_set('UTC');
 		array_push($json_expiry_date, $row['expiry_date']);
 		$row['count_puuid'] = (int) $row['count_puuid'];
@@ -31,14 +47,14 @@
 	/* mysql_close($link); 	*/
 	echo json_encode($expiry_date);
 	echo json_encode($count_puuid);
-	
+
 ?>
   <script type="text/javascript" src="exporting.js"></script>
-  <script type='text/javascript'> 
-  var points = <?php echo json_encode($count_puuid);?>,  
+  <script type='text/javascript'>
+  var points = <?php echo json_encode($count_puuid);?>,
   dates = <?php echo json_encode($expiry_date);?>,
   i, data = [], chart;
-  for(i=0; i<points.length; i++) {    
+  for(i=0; i<points.length; i++) {
     data.push([dates[i], points[i]]);
 								}
 	//$(function(){
@@ -60,7 +76,7 @@
 				day: '%e. %b',
 				week: '%e. %b',
 				month: '%b \'%y',
-				year: '%Y'      
+				year: '%Y'
 				},
 			maxZoom: 24 * 3600 * 1000
 				},
@@ -93,10 +109,9 @@
 			data: data,
 			pointInterval: 24 * 3600 * 1000//one day
 				}]
-	});  
+	});
 });
 </script>
 <!-- Add the container -->
 <div id="container" style="height: 400px;"></div>
 
- 
