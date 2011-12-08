85a654d7613999eb7e7975c58043f9644ee1d313
Greg Miernicki <g@miernicki.com>
2011-12-08 18:19:17 -0500

    updated unit tests for plus 21

--- vesuvius/mod/plus/unitTest.php
+++ vesuvius/mod/plus/unitTest.php
@@ -23,6 +23,8 @@ $uuid = "3";
 $mcid = "0";
 $newMcid = "1";
 $expiryDate = "2036-11-11 11:11:11";
+$tokenStart = "1";
+$tokenEnd = "1";
 
 // load nusoap client library
 require_once("../../3rd/nusoap/lib/nusoap.php");
@@ -57,7 +59,10 @@ if(!isset($_GET['api'])) {
 		case '2.2':
 			reReportPerson($uuid, $personXML, $eventShortname, $xmlFormat, $user, $pass);
 		case '2.1':
-
+			getImageCountsAndTokens($user, $pass);
+			getImageList($tokenStart, $tokenEnd, $user, $pass);
+			getImageListBlock($tokenStart, $user, $pass);
+			getNullTokenList($tokenStart, $tokenEnd, $user, $pass);
 		case '2.0':
 			expirePerson($uuid, '', $user, $pass);
 			getPersonExpiryDate($uuid);
