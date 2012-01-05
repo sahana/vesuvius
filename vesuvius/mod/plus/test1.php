18939f7d41126fd47e2f461a9bbf41d53a522f95
Greg Miernicki <g@miernicki.com>
2012-01-05 12:21:11 -0500

    reverted plus version #

--- vesuvius/mod/plus/test1.php
+++ vesuvius/mod/plus/test1.php
@@ -14,7 +14,7 @@ $user = "testDontDelete";
 $pass = "dontDelete99";
 require_once("../../3rd/nusoap/lib/nusoap.php");
 
-$wsdl = "https://pl.nlm.nih.gov/?wsdl&api=2.2";
+$wsdl = "https://pl.nlm.nih.gov/?wsdl&api=2.3";
 //$wsdl = "https://plstage.nlm.nih.gov/?wsdl&api=1.9.8";
 //$wsdl = "http://plstage.nlm.nih.gov/~miernickig/vesuvius/vesuvius/www/index.php?wsdl&api=2.2";
 $client = new nusoap_client($wsdl);
