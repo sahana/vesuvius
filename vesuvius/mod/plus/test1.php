c6d92fa549aee99199156b920d0cc45a36e05a16
Greg Miernicki <g@miernicki.com>
2012-01-25 13:03:01 -0500

    changed plus back to using SQL for search in 2.2

--- vesuvius/mod/plus/test1.php
+++ vesuvius/mod/plus/test1.php
@@ -15,9 +15,9 @@ $user = "testDontDelete";
 $pass = "dontDelete99";
 require_once("../../3rd/nusoap/lib/nusoap.php");
 
-$wsdl = "https://pl.nlm.nih.gov/?wsdl&api=2.2";
+//$wsdl = "https://pl.nlm.nih.gov/?wsdl&api=2.2";
 //$wsdl = "https://plstage.nlm.nih.gov/?wsdl&api=2.2";
-//$wsdl = "http://plstage.nlm.nih.gov/~miernickig/vesuvius/vesuvius/www/index.php?wsdl&api=2.2";
+$wsdl = "http://plstage.nlm.nih.gov/~miernickig/vesuvius/vesuvius/www/index.php?wsdl&api=2.2";
 $client = new nusoap_client($wsdl);
 
 //$result = $client->call('getNullTokenList', array('tokenStart'=>'0', 'tokenEnd'=>'120', 'username'=>$user, 'password'=>$pass));
