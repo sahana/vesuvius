c1cb043a9c169d9fc26afba5030d6a2d2ef02320
Greg Miernicki <g@miernicki.com>
2012-02-23 11:57:37 -0500

    pfif v2 gui with tabs

--- vesuvius/mod/pfif/routines.sql
+++ vesuvius/mod/pfif/routines.sql
@@ -1,8 +1,21 @@
+/**
+ * @name         Person Finder Interchange Format
+ * @version      2
+ * @package      pfif
+ * @author       Carl H. Cornwell <ccornwell at aqulient dor com>
+ * @author       Leif Neve <lneve@mail.nih.gov>
+ * @author       Greg Miernicki <g@miernicki.com> <gregory.miernicki@nih.gov>
+ * @about        Developed in whole or part by the U.S. National Library of Medicine
+ * @link         https://pl.nlm.nih.gov/about
+ * @license	 http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
+ * @lastModified 2012.0223
+ */
+
 --
 -- Delete a reported person and all related data and optionally also Notes.
 -- Optional Note deletion needed since some deletions are simply for correcting erroneous data in the person record
 -- and are NOT an attempt to expunge data.
--- Does not delete locally registered user. 
+-- Does not delete locally registered user.
 --
 
 CREATE PROCEDURE delete_reported_person(IN id VARCHAR(128),IN deleteNotes BOOLEAN)
@@ -18,10 +31,10 @@ DELETE person_uuid.* FROM person_uuid WHERE p_uuid = id;
 DELETE pfif_person.* FROM pfif_person WHERE p_uuid = id;
 
 IF deleteNotes THEN
-  -- Delete note from pfif_note 
+  -- Delete note from pfif_note
   DELETE pfif_note.* FROM pfif_note WHERE p_uuid = id;
 
-  -- Set to null linked records in pfif_note 
+  -- Set to null linked records in pfif_note
   UPDATE pfif_note SET linked_person_record_id = NULL WHERE linked_person_record_id = id;
 END IF;
 
