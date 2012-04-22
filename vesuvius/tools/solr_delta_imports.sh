78b08c4c8261f9df85e2f605f677f3823a456f72
Leif Neve <leifneve@gmail.com>
2012-04-22 08:56:44 -0400

    use server-indepent SOLR URL (PL-381)

--- vesuvius/tools/solr_delta_imports.sh
+++ vesuvius/tools/solr_delta_imports.sh
@@ -2,6 +2,6 @@
 # Perform SOLR delta updates forever. No logging (check /opt/solr/logs).
 while :
 do
-	/usr/bin/curl "http://pl.nlm.nih.gov:8983/solr/dataimport?command=delta-import" > /dev/null 2>&1
+	/usr/bin/curl "http://localhost:8983/solr/dataimport?command=delta-import" > /dev/null 2>&1
 	sleep 5
 done
